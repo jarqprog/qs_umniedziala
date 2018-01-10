0ada2b2ef7c0ca308fed3194140e4be42a8c323f
Joanna Baran <joannabaran56@gmail.com>
2018-01-10 18:18:38 +0100

    Artifact: add and implement constructors

--- model/Artifact.java
+++ model/Artifact.java
@@ -1,5 +1,30 @@
 package model;
 
-public class Artifact{
+public class Artifact extends Item {
+
+    public Artifact(String name,
+                 int value,
+                 String description,
+                 String status) {
+
+        this.name = name;
+        this.value = value;
+        this.description = description;
+        this.status = status;
+        this.itemId = ++idCounter;
+    }
+
+    public Artifact(String name,
+                 int value,
+                 String description,
+                 String status,
+                 int itemId) {
+
+        this.name = name;
+        this.value = value;
+        this.description = description;
+        this.status = status;
+        this.itemId = itemId;
+    }
     
 }
