aab108a8bd9d8af99d33e56930928f9603f3f957
martastaporek <staporekmarta1@gmail.com>
2018-01-10 16:46:04 +0100

    IDaoQuest(): Fill class

--- dao/IDaoQuest.java
+++ dao/IDaoQuest.java
@@ -1,5 +1,12 @@
 package dao;
 
+import model.*;
+import java.util.ArrayList;
+
 public interface IDaoQuest extends IDao{
+
+    public ArrayList<Quest> getTeams();
+    public Quest getTeamById();
+    public Quest createTeam();
 
 }
