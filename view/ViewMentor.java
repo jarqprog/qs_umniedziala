08f61f4618efc26804d84e694301e99a4ea33556
martastaporek <staporekmarta1@gmail.com>
2018-01-10 20:27:10 +0100

    ViewMentor(): Add option list

--- view/ViewMentor.java
+++ view/ViewMentor.java
@@ -1,5 +1,15 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
 public class ViewMentor extends View{
+    
+    private static ArrayList <String> mentorOptions = new ArrayList <String>(Arrays.asList("1.Create student",
+        "Create quest", "Edit mentor", "Split quests to categories", "Add artifact to store",
+        "Edit quest", "Edit artifact", "Split artifacts to categories", "Mark quest", "Mark artifact",
+        "See student's wallet"));
+
 
 }
