9741ea907264875069a7cad9ade3775e035964a9
martastaporek <staporekmarta1@gmail.com>
2018-01-10 20:16:05 +0100

    ViewAdmin(): Implement option list

--- view/ViewAdmin.java
+++ view/ViewAdmin.java
@@ -1,5 +1,13 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ViewAdmin extends View{
+
+    private static ArrayList <String> adminOptions = new ArrayList <String>(Arrays.asList("1.Create mentor",
+    "Create class", "Edit mentor", "See mentor's profile", "Create levels of experience"));
+
+    public void menu(){}
 
 }
