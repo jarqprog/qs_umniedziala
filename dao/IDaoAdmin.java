11ad0033d0207c687037bc36da60a058fd8047e3
martastaporek <staporekmarta1@gmail.com>
2018-01-10 19:28:36 +0100

    DaoQuest(): Implement basic func in class

--- dao/IDaoAdmin.java
+++ dao/IDaoAdmin.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
 public interface IDaoAdmin{
 
     public ArrayList<Admin> getAdmins();
-    public Admin getAdminById();
-    public Admin createAdmin();
+    public Admin getAdminById(int id);
+    public Admin createAdmin(String name, String password, String email);
     public void importData(Admin admin);
     public void exportData();
 
