1486ab7c66d907253b114b3ecaf5d847fef4c314
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-01-10 21:50:57 +0100

    Group: Create class frame

--- model/Group.java
+++ model/Group.java
@@ -1,5 +1,27 @@
 package model;
 
+import java.util.ArrayList;
+
 public abstract class Group{
+    protected String name;
+    protected ArrayList<Student> students;
+    protected int groupId;
+
+    public Group(){
+        ;
+    }
+
+    public Group(String name, ArrayList<Student> students, int groupId){
+        ;
+    }
+    public setGroupId(int groupId){}
+    public int getGroupId(){}
+    public setName(String name){}
+    public String getName(){}
+    public setStudents(ArrayLIst<Student> students){}
+    public ArrayList<Student> getStudents(){}
+    public boolean addMember(Student student){}
+    public boolean removeMember(Student student){}
+    public String toString(){}
     
 }
