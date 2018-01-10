ad0ee9fa28f36c1f46904d06fde7dbfb2728eb8c
martastaporek <staporekmarta1@gmail.com>
2018-01-10 16:21:03 +0100

    IDaoMentor(): Fill class

--- dao/IDaoMentor.java
+++ dao/IDaoMentor.java
@@ -1,5 +1,12 @@
 package dao;
 
+import model.*;
+import java.util.ArrayList;
+
 public interface IDaoMentor extends IDao{
+
+    public ArrayList<Mentor> getMentors();
+    public Mentor getMentorById(int id);
+    public void createMentor();
 
 }
