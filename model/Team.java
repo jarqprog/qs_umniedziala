c5435970a7021ba4f935acc7bd84d7560eab77eb
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-01-10 22:37:08 +0100

    Team: Create class frame

--- model/Team.java
+++ model/Team.java
@@ -1,5 +1,12 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Team extends Group{
-    
+    private int coins;
+
+    public CodecoolClass(String name){}
+    public CodecoolClass(String name, ArrayList<Student> students, int groupId, int coins){}
+    public void setCoins(int coins){}
+    public int getCoins(){}
 }
