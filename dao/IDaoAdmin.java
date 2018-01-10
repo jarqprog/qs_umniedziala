785f6b684be12608a674e459ee3c35dfe919544a
martastaporek <staporekmarta1@gmail.com>
2018-01-10 16:30:10 +0100

    IDaoAdmin: Fill class

--- dao/IDaoAdmin.java
+++ dao/IDaoAdmin.java
@@ -1,5 +1,12 @@
 package dao;
 
+import model.*;
+import java.util.ArrayList;
+
 public interface IDaoAdmin extends IDao{
+
+    public ArrayList<Admin> getAdmins();
+    public Admin getAdminById();
+    public Admin createAdmin();
 
 }
