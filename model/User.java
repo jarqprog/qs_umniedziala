333548ed1b96589ca86ec822b9812784da4afd28
filipHartman <filip.hartman23@gmail.com>
2018-01-10 16:11:22 +0100

    User: Initalize class

--- model/User.java
+++ model/User.java
@@ -1,5 +1,53 @@
 package model;
 
 public abstract class User{
+    protected String name;
+    protected String password;
+    protected String email;
+    protected int userId;
+    protected static int COUNTER;
 
+    public User(String name, String password, String email) {
+
+    }
+
+    public User(String name, String password, String email, int userId) {
+
+    }
+
+    public String getName() {
+
+    }
+
+    public String getPassword() {
+
+    }
+
+    public String getEmail() {
+
+    }
+
+    public int getUserId() {
+
+    }
+
+    public void setName(String name) {
+
+    }
+
+    public void setPassword(String password) {
+
+    }
+
+    public void setEmail(String email) {
+
+    }
+
+    public setIdCounter(int id) {
+
+    }
+
+    public String toString() {
+        
+    }
 }
