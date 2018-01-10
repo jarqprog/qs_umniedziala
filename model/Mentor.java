4531722e424298a67f204ad12fc9836608cc2611
filipHartman <filip.hartman23@gmail.com>
2018-01-10 16:41:36 +0100

    Mentor: Add constructors of class

--- model/Mentor.java
+++ model/Mentor.java
@@ -1,5 +1,14 @@
 package model;
 
 public class Mentor extends User{
+    private int classId;
 
+    public Mentor(String name, String password, String email) {
+        super(name, password, email);
+    }
+
+    public Mentor(String name, String password, String email, int userId, int classId) {
+        super(name, password, email, userId);
+        this.classId = classId;
+    }
 }
