34064e7a5ea1f643d8ae5a2681d2f0535eaa5cc1
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-01-10 19:05:53 +0100

    Wallet: Create class frame

--- model/Wallet.java
+++ model/Wallet.java
@@ -1,5 +1,19 @@
 package model;
 
+import model.Artifact;
+
 public class Wallet{
-    
+    private int coins;
+    private int balance;
+    private ArrayList <Artifacts> artifacts;
+
+    public Wallet(){}
+    public Wallet(int balance, int coins, ArrayList <Artifacts> artifacts){}
+    public setCoins(int coins){}
+    public int getCoins(){}
+    public setBalance(int balance){}
+    public int getBalance(){}
+    public setArtifacts(ArrayList <Artifacts> artifacts){}
+    public ArrayList <Artifacts> getArtifacts(){}
+    public String toString(){}
 }
