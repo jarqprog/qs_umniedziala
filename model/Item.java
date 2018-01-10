6fb221ff9d51bbd63dce5a13d2d2ae3057746f8d
Joanna Baran <joannabaran56@gmail.com>
2018-01-10 16:21:26 +0100

    Item: define attributes

--- model/Item.java
+++ model/Item.java
@@ -1,5 +1,11 @@
 package model;
 
 public abstract class Item{
+
+    protected String name;
+    protected int value;
+    protected String description;
+    protected boolean status;
+    protected int itemId;
     
 }
