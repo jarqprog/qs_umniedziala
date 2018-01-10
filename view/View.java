559c782511abc43bfafa98df8c1cff0ee5e0c5c7
martastaporek <staporekmarta1@gmail.com>
2018-01-10 20:01:55 +0100

    View(): Implement class

--- view/View.java
+++ view/View.java
@@ -1,5 +1,38 @@
 package view;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.stream.*;
+import java.util.ArrayList;
+
+
 public abstract class View{
+
+    public String getInputFromUser(String request){
+        try {
+            System.out.println(request);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String userInput = reader.readLine();
+            return userInput;
+            
+            } 
+        catch (IOException e) {
+            System.out.println("Wrong input");
+            }  
+        return null;    
+    }
+
+    public void displayText(String text){
+        System.out.println(text);
+    }
+
+    public void displayList(ArrayList <String> list){
+        for(String text: list){
+            System.out.println(text);
+        }
+    }
+    public abstract void menu();
+
     
 }
