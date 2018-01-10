8a1af74439a5729cbbdd36896e945f96e63e435c
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-01-10 20:44:28 +0100

    myIterator: Create class frame

--- iterator/myIterator.java
+++ iterator/myIterator.java
@@ -3,5 +3,15 @@ package iterator;
 import java.util.ArrayList;
 
 public class myIterator <T> implements IIterator{
-    ;
+    private int index = 0;
+    private ArrayList<T> collection;
+    
+    public myIterator(ArrayList<T> collection){
+    }
+    
+    public boolean hasNext(){
+    }
+    
+    public T next(){
+    }
 }
