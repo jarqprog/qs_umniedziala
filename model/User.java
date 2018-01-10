795a8472af580f75c0d19d2a976571ffd9e427e2
filipHartman <filip.hartman23@gmail.com>
2018-01-10 16:24:51 +0100

    User: Add body of getter and setter methods

--- model/User.java
+++ model/User.java
@@ -8,46 +8,56 @@ public abstract class User{
     protected static int COUNTER;
 
     public User(String name, String password, String email) {
-
+        this.name = name;
+        this.password = password;
+        this.email = email;
     }
 
     public User(String name, String password, String email, int userId) {
-
+        User(name, password, email);
+        this.userId = userId;
     }
 
     public String getName() {
-
+        return name;
     }
 
     public String getPassword() {
-
+        return password;
     }
 
     public String getEmail() {
-
+        return email;
     }
 
     public int getUserId() {
-
+        return userId;
     }
 
     public void setName(String name) {
-
+        this.name = name;
     }
 
     public void setPassword(String password) {
-
+        this.password = password;
     }
 
     public void setEmail(String email) {
-
+        this.email = email;
     }
 
-    public setIdCounter(int id) {
+    public static void setIdCounter(int id) {
 
     }
 
     public String toString() {
-        
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Name: " + getName());
+        sb.append("\nEmail: " + getEmail());
+        sb.append("\nID: " + getUserId());
+
+        return sb.toString();
+
     }
 }
