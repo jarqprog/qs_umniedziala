0a9b1e74d900bcc4670bce0c90e5ffbc71e8175c
martastaporek <staporekmarta1@gmail.com>
2018-01-10 16:35:45 +0100

    IDaoClass(): fill class

--- dao/IDaoClass.java
+++ dao/IDaoClass.java
@@ -1,5 +1,12 @@
 package dao;
 
+import model.*;
+import java.util.ArrayList;
+
 public interface IDaoClass extends IDao{
+
+    public ArrayList<CodecoolClass> getTeams();
+    public CodecoolClass getTeamById();
+    public CodecoolClass createTeam();
 
 }
