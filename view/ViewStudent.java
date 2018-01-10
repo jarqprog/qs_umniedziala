ada9c7ae952ef92f4576141949e785242bfd0480
martastaporek <staporekmarta1@gmail.com>
2018-01-10 20:34:03 +0100

    ViewTeam(): Add option list

--- view/ViewStudent.java
+++ view/ViewStudent.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 public class ViewStudent extends View{
 
-    private static ArrayList <String> studentOptions = new ArrayList <String>(Arrays.asList("See wallet details",
-        "2. Buy artifact"));
+    private static ArrayList <String> studentOptions = new ArrayList <String>(Arrays.asList("1.See wallet details",
+        "2. Buy artifact", "3. Check experience level"));
     
 }
--- view/ViewTeam.java
+++ view/ViewTeam.java
@@ -1,5 +1,11 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ViewTeam extends View{
+
+    private static ArrayList <String> studentOptions = new ArrayList <String>(Arrays.asList("1.Buy artifact",
+        "2. Split money"));
     
 }
