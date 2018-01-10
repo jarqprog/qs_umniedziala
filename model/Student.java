b15865b259f0415a5c13aca31b9143293b63a260
filipHartman <filip.hartman23@gmail.com>
2018-01-10 17:17:40 +0100

    Student: Add constructors of class

--- model/Student.java
+++ model/Student.java
@@ -1,5 +1,17 @@
 package model;
 
 public class Student extends User{
+    private int classId;
+    private int teamId;
+    private Wallet wallet;
 
+    public Student(String name, String password, String email, int classId) {
+        super(name, password, email);
+        this.classId = classId;
+    }
+
+    public Student(String name, String password, String email, int classId, int teamId) {
+        this(name, password, email, classId);
+        this.teamId = teamId;
+    }
 }
