78a33ea5511f6ac237b60d90c3fcebbbbb99a292
martastaporek <staporekmarta1@gmail.com>
2018-01-10 20:31:29 +0100

    ViewStudent(): Add option list

--- view/ViewStudent.java
+++ view/ViewStudent.java
@@ -1,5 +1,11 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ViewStudent extends View{
+
+    private static ArrayList <String> studentOptions = new ArrayList <String>(Arrays.asList("See wallet details",
+        "2. Buy artifact"));
     
 }
