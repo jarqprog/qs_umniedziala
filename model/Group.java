0a6fc24d0d463d4287f193c44642b0d54b982321
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-01-10 21:57:13 +0100

    Group: Fill Group(String) and Group(String, ArrayList<Student>, int)

--- model/Group.java
+++ model/Group.java
@@ -6,13 +6,18 @@ public abstract class Group{
     protected String name;
     protected ArrayList<Student> students;
     protected int groupId;
+    protected static int counterId = 0;
 
-    public Group(){
-        ;
+    public Group(String name){
+        this.name = name;
+        this.students = new ArrayList<Student>();
+        this.groupId = ++counterId;
     }
 
     public Group(String name, ArrayList<Student> students, int groupId){
-        ;
+        this.name = name;
+        this.students = students;
+        this.groupId = groupId;
     }
     public setGroupId(int groupId){}
     public int getGroupId(){}
