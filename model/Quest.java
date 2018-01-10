597286a3cb9fe09511c272cc9f1d30b5889b91eb
Joanna Baran <joannabaran56@gmail.com>
2018-01-10 17:56:46 +0100

    Quest: Add and implement constructors

--- model/Quest.java
+++ model/Quest.java
@@ -1,5 +1,36 @@
 package model;
 
-public class Quest{
+public class Quest extends Item {
+
+    private String type;
+
+    public Quest(String name,
+                 int value,
+                 String description,
+                 String status,
+                 String type) {
+
+        this.name = name;
+        this.value = value;
+        this.description = description;
+        this.status = status;
+        this.type = type;
+        this.itemId = ++idCounter;
+    }
+
+    public Quest(String name,
+                 int value,
+                 String description,
+                 String status,
+                 String type,
+                 int itemId) {
+
+        this.name = name;
+        this.value = value;
+        this.description = description;
+        this.status = status;
+        this.type = type;
+        this.itemId = itemId;
+    }
 
 }
