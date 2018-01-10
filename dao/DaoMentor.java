088c58f74fb4935d293a3db13b5f0f18167aa183
martastaporek <staporekmarta1@gmail.com>
2018-01-10 19:22:51 +0100

    DaoMentr(): Implement basic func in class

--- dao/DaoMentor.java
+++ dao/DaoMentor.java
@@ -1,5 +1,32 @@
 package dao;
 
+import model.*;
+import java.util.ArrayList;
+
+
 public class DaoMentor implements IDaoMentor{
+
+    private static ArrayList <Mentor> mentors;
+
+    public ArrayList <Mentor> getMentors() { return mentors; }
+
+    public Mentor createMentor(String name, String password, String email){
+        Mentor mentor = new Mentor(name, password, email);
+        return mentor;
+    }
+
+    public Mentor getMentorById(int id){
+        for(Mentor mentor: mentors){
+            if(mentor.getUserId() == id){
+                return mentor;
+            }
+        }
+        return null;
+    }
+
+    public void exportData(){  }
+    public void importData(Mentor mentor){
+        mentors.add(mentor);
+    }
 
 }
--- dao/IDaoMentor.java
+++ dao/IDaoMentor.java
@@ -7,7 +7,7 @@ public interface IDaoMentor {
 
     public ArrayList<Mentor> getMentors();
     public Mentor getMentorById(int id);
-    public Mentor createMentor();
+    public Mentor createMentor(String name, String password, String email);
     public void importData(Mentor mentor);
     public void exportData();
 
