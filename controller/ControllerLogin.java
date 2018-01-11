ed850f91e8e764b11c8a2b2382bf5b1eecfe41f6
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-01-11 10:22:42 +0100

    ControllerLogin: Create class frame

--- controller/ControllerLogin.java
+++ controller/ControllerLogin.java
@@ -1,5 +1,23 @@
 package controller;
 
+import java.util.ArrayList;
+import model.User;
+
 public class ControllerLogin{
-    ;
+
+    public void login(){
+        ;
+    }
+
+    private ArrayList<User> getAllUsers(){
+        ;
+    }
+
+    private User getUser(String email, String password){
+        ;
+    }
+
+    private IUserController getUserController(User user){
+        ;
+    }
 }
