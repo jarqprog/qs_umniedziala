e121115acb6cf569f7e3a872aff2fa3f53447703
Joanna Baran <joannabaran56@gmail.com>
2018-01-11 11:46:22 +0100

    ViewStudent: modify student options and add its getter

--- view/ViewStudent.java
+++ view/ViewStudent.java
@@ -7,7 +7,12 @@ import java.util.List;
 
 public class ViewStudent extends View{
 
-    private static List <String> studentOptions = new ArrayList <String>(Arrays.asList("1.See wallet details",
-        "2. Buy artifact", "3. Check experience level"));
+    private static ArrayList <String> studentOptions = new ArrayList <String>(Arrays.asList("1. See wallet details",
+        "2. Buy artifact", "3. Check experience level", "4. Manage team", "5. See available quests",
+        "6. See available artifacts", "0. Quit"));
+
+    public ArrayList<String> getStudentOptions() {
+        return studentOptions;
+    }
     
 }
