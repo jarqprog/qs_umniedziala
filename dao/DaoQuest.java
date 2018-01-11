1831120028e74f69ed2fae495cd4873ef04aa9c7
martastaporek <staporekmarta1@gmail.com>
2018-01-11 11:02:11 +0100

    DaoQuest: fill class

--- dao/DaoQuest.java
+++ dao/DaoQuest.java
@@ -1,5 +1,30 @@
 package dao;
 
+import model.*;
+import java.util.ArrayList;
+
 public class DaoQuest implements IDaoQuest{
+
+    private static ArrayList <Quest> quests;
+
+    public ArrayList <Quest> getQuests() { return quests; }
+
+    public Quest createQuest(String name, int value, String description, String status, String type){
+        Quest quest = new Quest(name, value, description, status, type);
+        return quest;
+    }
+
+    public Quest getQuestById(int id){
+        for(Quest quest: quests){
+            if(quest.getItemId() == id){
+                return quest;
+            }
+        }
+        return null;
+    }
+    public void exportData(){ }
+    public void importData(Quest quest){
+        quests.add(quest);
+    }
     
 }
--- dao/IDaoQuest.java
+++ dao/IDaoQuest.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 
 public interface IDaoQuest{
 
-    public ArrayList<Quest> getQuest();
-    public Quest getQuestById();
-    public Quest createQuest();
+    public ArrayList<Quest> getQuests();
+    public Quest getQuestById(int id);
+    public Quest createQuest(String name, int value, String description, String status, String type);
     public void importData(Quest quest);
     public void exportData();
 
