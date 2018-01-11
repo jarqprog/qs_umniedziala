c002b57a85a86d4efc3e93b92b405e9b4aecfeb1
Joanna Baran <joannabaran56@gmail.com>
2018-01-11 14:50:03 +0100

    ControllerAdmin: add constructor

--- controller/ControllerAdmin.java
+++ controller/ControllerAdmin.java
@@ -1,5 +1,16 @@
 package controller;
 
+import model.Admin;
+import view.ViewAdmin;
+
 public class ControllerAdmin{
-    ;
+
+    private ViewAdmin viewAdmin;
+    private Admin admin;
+
+    public ControllerAdmin(Admin admin) {
+        this.viewAdmin = new ViewAdmin();
+        this.admin = admin;
+    }
+
 }
