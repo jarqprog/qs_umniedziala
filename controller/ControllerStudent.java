f5b4ebe3f795362e86a8715112368d50eaeac5a6
Joanna Baran <joannabaran56@gmail.com>
2018-01-11 10:44:57 +0100

    ControllerStudent: add and implement constructor

--- controller/ControllerStudent.java
+++ controller/ControllerStudent.java
@@ -1,5 +1,16 @@
 package controller;
 
+import model.Student;
+import view.ViewStudent;
+
 public class ControllerStudent{
-    ;
+
+    private ViewStudent viewStudent;
+    private Student student;
+
+    public ControllerStudent(Student student) {
+        this.viewStudent = new ViewStudent();
+        this.student = student;
+    }
+
 }
