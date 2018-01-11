19300bec510ea0ac99f6029a509f3eef7c1fcadb
martastaporek <staporekmarta1@gmail.com>
2018-01-11 15:18:46 +0100

    DaoTeam/IDaoTeam: Fix import and export methods

--- dao/DaoTeam.java
+++ dao/DaoTeam.java
@@ -7,10 +7,15 @@ public class DaoTeam implements IDaoTeam{
 
     public static ArrayList <Team> teams;
 
-    public ArrayList<Team> getTeams() { return teams;}
     public Team getTeamById() {return teams.get(0);}
-    public Team createTeam() {return teams.get(0);}
-    public void importData(Team team) {}
-    public void exportData() {}
+    public void createTeam(String name) {
+        teams.add(new Team(name));
+    }
+    public ArrayList <Team> importData() {
+        return teams;
+    }
+    public void exportData(ArrayList <Team> updatedTeams) {
+        teams = updatedTeams;
+    }
     
 }
