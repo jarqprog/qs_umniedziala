487fbb4281107a6a68a47638e1216c50a06ac02c
martastaporek <staporekmarta1@gmail.com>
2018-01-11 15:32:40 +0100

    DaoClass/IDaoClass: Fill class

---
+++ dao/DaoClass.java
@@ -0,0 +1,31 @@
+package dao;
+
+import java.util.ArrayList;
+import model.*;
+
+public class DaoClass implements IDaoClass{
+
+    private static ArrayList <CodecoolClass> classes;
+
+    public void createClass(){
+        classes.add(new CodecoolClass());
+    }
+
+    public CodecoolClass getClassById(int id){
+        for(CodecoolClass codecoolClass: classes){
+            if(codecoolClass.getId() == id){
+                return codecoolClass;
+            }
+        }
+        return null;
+    }
+
+    public void exportData(ArrayList <CodecoolClass> updatedCodecoolClass){
+        classes = updatedCodecoolClass;
+    }
+
+    public ArrayList <CodecoolClass> importData(){
+        return classes;
+    }
+
+}
--- dao/IDaoClass.java
+++ dao/IDaoClass.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 
 public interface IDaoClass{
 
-    public ArrayList<CodecoolClass> getTeams();
     public CodecoolClass getTeamById();
     public CodecoolClass createTeam();
     public void importData(CodecoolClass codecoolclass);
