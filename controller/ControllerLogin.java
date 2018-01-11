b41c3d5fe136838c6ef70d77c2b98e4ceca0eb0e
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-01-11 10:54:25 +0100

    ControllerLogin: Fill getUser()

--- controller/ControllerLogin.java
+++ controller/ControllerLogin.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import dao.DaoAdmin;
 import dao.DaoMentor;
 import dao.DaoStudent;
+import iterator.MyIterator;
 import model.User;
 
 public class ControllerLogin{
@@ -15,6 +16,7 @@ public class ControllerLogin{
 
     private ArrayList<User> getAllUsers(){
         ArrayList <User> users = new ArrayList<>();
+        
         DaoAdmin daoAdmin = new DaoAdmin();
         DaoMentor daoMentor = new DaoMentor();
         DaoStudent daoStudent = new DaoStudent();
@@ -27,7 +29,16 @@ public class ControllerLogin{
     }
 
     private User getUser(String email, String password){
-        ;
+        ArrayList <User> users = getAllUsers();
+        MyIterator <User> myIterator = new MyIterator<>(users);
+
+        while(myIterator.hasNext()){
+            User user = myIterator.next();
+            if(user.getEmail().equals(email) && user.getPassword().equals(password)){
+                return user;
+            }
+        }
+        return null;
     }
 
     private IUserController getUserController(User user){
