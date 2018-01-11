a19141e2400089977d271a09d89efcad0703e545
Joanna Baran <joannabaran56@gmail.com>
2018-01-11 22:00:43 +0100

    IDaoMentor: modify method createMentor() signature

--- dao/IDaoMentor.java
+++ dao/IDaoMentor.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public interface IDaoMentor {
 
     public Mentor getMentorById(int id);
-    public void createMentor(String name, String password, String email);
+    public void createMentor(String name, String password, String email, int userId, int classId);
     public ArrayList <Mentor> importData();
     public void exportData(ArrayList <Mentor> updatedMenotrs);
 
