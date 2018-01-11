a3d7275a0b74f4cb49d0e62ff5f05b71dfa16de5
Joanna Baran <joannabaran56@gmail.com>
2018-01-11 15:43:11 +0100

    ViewAdmin: add space in admin options

--- view/ViewAdmin.java
+++ view/ViewAdmin.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class ViewAdmin extends View{
 
-    private static ArrayList <String> adminOptions = new ArrayList <String>(Arrays.asList("1.Create mentor",
+    private static ArrayList <String> adminOptions = new ArrayList <String>(Arrays.asList("1. Create mentor",
     "2. Create class", "3. Edit mentor", "4. See mentor's profile","5. Assign mentor to class",
     "6. Create levels of experience", "0. Quit"));
 
