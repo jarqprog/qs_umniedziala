f3b9e5aa88ea564b505919ed24b78bf2e0d24603
Joanna Baran <joannabaran56@gmail.com>
2018-01-11 15:09:50 +0100

    ViewAdmin: modify admin options and create its getter

--- view/ViewAdmin.java
+++ view/ViewAdmin.java
@@ -7,7 +7,12 @@ import java.util.List;
 
 public class ViewAdmin extends View{
 
-    private static List <String> adminOptions = new ArrayList <String>(Arrays.asList("1.Create mentor",
-    "2. Create class", "3. Edit mentor", "4. See mentor's profile", "5. Create levels of experience"));
+    private static ArrayList <String> adminOptions = new ArrayList <String>(Arrays.asList("1.Create mentor",
+    "2. Create class", "3. Edit mentor", "4. See mentor's profile","5. Assign mentor to class",
+    "6. Create levels of experience", "0. Quit"));
+
+    public ArrayList<String> getAdminOptions() {
+        return adminOptions;
+    }
 
 }
