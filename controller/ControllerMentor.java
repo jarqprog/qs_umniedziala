1417fb89ca50c43a18cd6c92dbf95fd53938448a
filipHartman <filip.hartman23@gmail.com>
2018-01-11 11:08:14 +0100

    ControllerMentor: Add heading of the methods

--- controller/ControllerMentor.java
+++ controller/ControllerMentor.java
@@ -1,16 +1,70 @@
 package controller;
 import view.ViewMentor;
 import dao.*;
+import model.Mentor;
 
 public class ControllerMentor{
     private ViewMentor viewMentor;
+    private Mentor mentor;
 
-    public ControllerMentor(){
+    public ControllerMentor(Mentor mentor){
         this.viewMentor = new ViewMentor();
+        this.mentor = mentor;
     }
-    
+
+    public void createStudent() {
+
+    }
+
+    public void createTeam() {
+
+    }
+
     public void addQuest(){
 
     }
 
+    public void addArtifact() {
+
+    }
+
+    public void updateQuest() {
+
+    }
+
+    public void updateArtifact() {
+
+    }
+
+    public void markStudentAchivedQuest() {
+
+    }
+
+    public void markTeamAchivedQuest() {
+
+    }
+
+    public void markStudentBoughtArtifact() {
+
+    }
+
+    public void markTeamBoughtArtifact() {
+
+    }
+
+    public void seeAllWallets() {
+
+    }
+
+    public void seeQuests() {
+
+    }
+
+    public void seeArtifacts() {
+
+    }
+
+    public void runMenu() {
+        
+    }
 }
