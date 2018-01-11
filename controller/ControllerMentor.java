6bd2898a2fbe0a47e565f7a634b9738becf7cc1f
filipHartman <filip.hartman23@gmail.com>
2018-01-11 13:48:27 +0100

    ControllerMentor: Creating new team (not completed because daoteam is not finished

--- controller/ControllerMentor.java
+++ controller/ControllerMentor.java
@@ -2,10 +2,10 @@ package controller;
 
 import view.ViewMentor;
 import dao.*;
-import model.Mentor;
+import model.*;
 
 import java.util.ArrayList;
- 
+
 public class ControllerMentor{
     private ViewMentor viewMentor;
     private Mentor mentor;
@@ -16,11 +16,32 @@ public class ControllerMentor{
     }
 
     public void createStudent() {
+        DaoStudent daoStudent = new DaoStudent();
 
+        String nameRequest = "Enter name of new student: ";
+        String studentName = viewMentor.getInputFromUser(nameRequest);
+
+        String passwordRequest = "Enter password of new student: ";
+        String studentPassword = viewMentor.getInputFromUser(passwordRequest);
+
+        String emailRequest = "Enter email of new student: ";
+        String studentEmail = viewMentor.getInputFromUser(emailRequest);
+
+        String classIdRequest = "Enter ID of class which student will attend: ";
+        int classId = viewMentor.getIntInputFromUser(classIdRequest);
+
+        Student newStudent = daoStudent.createStudent(studentName, studentPassword, studentEmail, classId);
+
+        daoStudent.export; // skończ to !!!!!!!!!!!!!!!!!!!!
     }
 
     public void createTeam() {
+        DaoTeam daoTeam = new DaoTeam();
 
+        String nameRequest = "Enter name of new team: ";
+        String teamName = viewMentor.getInputFromUser(nameRequest);
+
+        // jakieś metody w dao do tworzenia nowego teamu i exportowanie go 
     }
 
     public void addQuest(){
