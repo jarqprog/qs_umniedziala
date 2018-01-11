49c692c618ea348f20e0a35453b3b05eb0c807ab
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-01-11 10:44:34 +0100

    ControllerLogin: Fill getAllUsers()

--- controller/ControllerLogin.java
+++ controller/ControllerLogin.java
@@ -1,6 +1,10 @@
 package controller;
 
 import java.util.ArrayList;
+
+import dao.DaoAdmin;
+import dao.DaoMentor;
+import dao.DaoStudent;
 import model.User;
 
 public class ControllerLogin{
@@ -10,7 +14,16 @@ public class ControllerLogin{
     }
 
     private ArrayList<User> getAllUsers(){
-        ;
+        ArrayList <User> users = new ArrayList<>();
+        DaoAdmin daoAdmin = new DaoAdmin();
+        DaoMentor daoMentor = new DaoMentor();
+        DaoStudent daoStudent = new DaoStudent();
+
+        users.addAll(daoAdmin.getAdmins());
+        users.addAll(daoMentor.getAdmins());
+        users.addAll(daoStudent.getAdmins());
+        
+        return users;
     }
 
     private User getUser(String email, String password){
