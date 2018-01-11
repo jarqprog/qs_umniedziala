af121abd0f41e52b120c83f178e12d28626409a8
martastaporek <staporekmarta1@gmail.com>
2018-01-11 19:32:50 +0100

    ViewMentor: Fix option list for mentor

--- view/ViewMentor.java
+++ view/ViewMentor.java
@@ -7,8 +7,9 @@ import java.util.Arrays;
 public class ViewMentor extends View{
 
     private static ArrayList <String> mentorOptions = new ArrayList <String>(Arrays.asList("1. Create student",
-        "2. Create quest", "3. Edit mentor", "4. Add artifact to store", "5. Edit quest",
-        "6. Edit artifact", "7. Mark quest", "8. Mark artifact", "9. See student's wallet"));
+        "2. Create team", "3. Create quest", "4. Add artifact to store", "5. Edit quest",
+        "6. Edit artifact", "7. Mark quest", "8. Mark artifact", "9. See student's wallet",
+        "0. Exit"));
 
     public ArrayList<String> getMentorOptions() {
         return mentorOptions;
