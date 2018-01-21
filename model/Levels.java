eba04d890aabd461e80ba5d0f48d6e17077e848d
martastaporek <staporekmarta1@gmail.com>
2018-01-21 15:29:57 +0100

    Levels: Fill class with constructor, getters ans setters

--- model/Levels.java
+++ model/Levels.java
@@ -1,5 +1,38 @@
 package model;
 
 public class Levels{
+
+    private String name;
+    private int coins;
+    private int id;
+    private static int idCounter;
+
+    public void Levels(String name, int coins){
+        
+        this.name = name;
+        this.coins = coins;
+        this.id = ++idCounter;
+    }
     
+    public String getName() { return this.name;}
+    public int getCoins() {return this.coins;}
+    public int getId() {return this.id;}
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setCoins(int coins){
+        this.coins = coins;
+    }
+
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("ID: " + getId());
+        sb.append(", Name: " + getName());
+        sb.append(", Email: " + getCoins());
+
+        return sb.toString();
+    }
 }
