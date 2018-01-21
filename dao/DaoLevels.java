d378ea69bffab4c7ade4a43d4f4490631d666c3d
martastaporek <staporekmarta1@gmail.com>
2018-01-21 15:34:47 +0100

    DaoLevels/IDaoLevels: Implement method create

--- dao/DaoLevels.java
+++ dao/DaoLevels.java
@@ -7,8 +7,12 @@ public class DaoLevels implements IDaoLevels{
 
     private static ArrayList <Levels> levels = new ArrayList<>();
 
+    public void createLevels(String name, int coins) {
+        levels.add(new Levels(name, coins));
+    }
+
     public Levels getLevelById(int id) {return levels.get(0);}
-    public void createLevels() { }
+    
     public ArrayList <Levels> importData() {return levels;}
     public void exportData(ArrayList <Levels> list) {}
 
