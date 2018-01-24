7073e6d9653038809e1548c5a58721ec91e7e875
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-01-25 00:46:41 +0100

    Student: Update constructors on Wallet objects

--- src/model/Student.java
+++ src/model/Student.java
@@ -3,12 +3,16 @@ package model;
 public class Student extends User{
     private Wallet wallet;
 
-    public Student(int userId, String name, String password, String email) {
+    public Student(int userId, String name, String password, String email, Wallet wallet) {
+
         super(userId, name, password, email);
+        this.wallet = wallet;
     }
 
     public Student(String name, String password, String email) {
+
         super(name, password, email);
+        this.wallet = new Wallet();
     }
 
     public Wallet getWallet() {
