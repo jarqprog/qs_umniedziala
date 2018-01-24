c3eac246db250b4a746042c085709beb57ef1aa7
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-01-24 15:06:22 +0100

    Quest: Remove unnecesary fields and methods

--- src/model/Quest.java
+++ src/model/Quest.java
@@ -2,40 +2,40 @@ package model;
 
 public class Quest extends Item {
 
-    private String type;
+    private String category;
 
     public Quest(String name,
                  int value,
                  String description,
-                 String status,
-                 String type) {
-
-        super(name, value, description, status);
-        this.type = type;
-    }
-
-    public Quest(String name,
-                 int value,
-                 String description,
-                 String status,
                  String type,
-                 int itemId) {
+                 String category) {
 
-        super(name, value, description, status, itemId);
-        this.type = type;
+        super(name, value, description, type);
+        this.category = category;
     }
 
-    public String getType() {
-        return type;
+    public Quest(int itemId,
+                 String name,
+                 int value,
+                 String description,
+                 String type,
+                 String category) {
+
+        super(itemId, name, value, description, type);
+        this.category = category;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 
     @Override
     public String toString() {
-        return "Quest: " + super.toString() + ", type: " + type;
+        return super.toString() + ", category: " + this.category;
     }
 
 }
