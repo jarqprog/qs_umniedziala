8a9c0ef160226af2004ee1d7f65ad689aaab2a0f
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-01-24 13:27:34 +0100

    Admin: Remove unnecesary fields and methods

--- src/model/Admin.java
+++ src/model/Admin.java
@@ -4,4 +4,8 @@ public class Admin extends User{
     public Admin(String name, String password, String email) {
         super(name, password, email);
     }
+
+    public Admin(int userId, String name, String password, String email) {
+        super(userId, name, password, email);
+    }
 }
