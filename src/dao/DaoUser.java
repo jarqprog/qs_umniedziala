7ddf570727f0a560e2eb6f73ab7669dbcc9854fd
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-01-24 23:40:06 +0100

    DaoUser:Create and fill getWallet() and createUser()

--- src/dao/DaoUser.java
+++ src/dao/DaoUser.java
@@ -1,9 +1,9 @@
 package dao;
 
-import model.Student;
-import model.User;
+import model.*;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 
 public class DaoUser {
@@ -37,13 +37,10 @@ public class DaoUser {
             statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(query);
 
-            int id = resultSet.getInt("id_user");
-            String name = resultSet.getString("name");
-            String password = resultSet.getString("password");
-            String email = resultSet.getString("email");
             int id_role = resultSet.getInt("id_role");
+            String role = getRole(id_role);
 
-            user = new Student(id, name, password, email);
+            user = createUser(resultSet, role);
 
             resultSet.close();
             statement.close();
@@ -60,7 +57,9 @@ public class DaoUser {
         try {
             statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("Select name from roles where id_role=" + id_role + ";");
+
             role = resultSet.getString("name");
+
             resultSet.close();
             statement.close();
         } catch (SQLException e) {
@@ -68,5 +67,54 @@ public class DaoUser {
         }
 
         return role;
+    }
+
+    public User createUser(ResultSet resultSet, String role){
+        User user = null;
+
+        int id = resultSet.getInt("id_user");
+        String name = resultSet.getString("name");
+        String password = resultSet.getString("password");
+        String email = resultSet.getString("email");
+
+        switch(role.toUpperCase()){
+            case "ADMIN":
+                user = Admin(id, name, password, email);
+                break;
+            case "MENTOR":
+                user = Mentor(id, name, password, email);
+                break;
+            case "STUDENT":
+                Wallet wallet = getWallet();
+                break;
+        }
+
+        return user;
+    }
+
+    private Wallet getWallet(int userID) {
+        Wallet wallet = null;
+        Statement statement = null;
+        ArrayList<Artifact> artifacts = null;
+        try {
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("Select * from wallets where id_student=" + userID + ";");
+
+            int allCoins = resultSet.getInt("all_coins");
+            int availableCoins = resultSet.getInt("available_coins");
+            artifacts = getUserArtifacts(userID);
+
+            wallet = new Wallet(allCoins, availableCoins, artifacts);
+
+            resultSet.close();
+            statement.close();
+        } catch (SQLException e) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+
+        return wallet;
+    }
+
+    private ArrayList<Artifact> getUserArtifacts(int userID) {
     }
 }
