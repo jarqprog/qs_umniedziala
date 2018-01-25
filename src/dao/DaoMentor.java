f2ba814bd714897772b06995072a7b7bac44c4dd
Joanna Baran <joannabaran56@gmail.com>
2018-01-25 18:29:28 +0100

    DaoMentor: add and implement method importInstance

--- src/dao/DaoMentor.java
+++ src/dao/DaoMentor.java
@@ -1,7 +1,10 @@
 package dao;
 
 import model.*;
-import java.util.ArrayList;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DaoMentor extends Dao {
 
@@ -13,5 +16,28 @@ public class DaoMentor extends Dao {
         return new Mentor(userId, name, password, email);
     }
 
+    public Mentor importInstance(int mentorId) {
+        Mentor mentor = null;
+        PreparedStatement preparedStatement = null;
+        String query = "SELECT * FROM users WHERE id_user = ?;";
+
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, mentorId);
+            ResultSet resultSet = preparedStatement.executeQuery(query);
+            int userId = resultSet.getInt("id_user");
+            String name = resultSet.getString("name");
+            String password = resultSet.getString("password");
+            String email = resultSet.getString("email");
+            resultSet.close();
+            preparedStatement.close();
+
+            mentor = createMentor(userId, name, password, email);
+
+        } catch (SQLException e) {
+            return mentor;
+        }
+        return mentor;
+    }
 
 }
