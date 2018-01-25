1a8a2bbe9bb6bc43fdd3b90e154cbbba5988d56d
Joanna Baran <joannabaran56@gmail.com>
2018-01-25 18:00:19 +0100

    DbConnection: Change way of return variable by method

--- src/dao/DbConnection.java
+++ src/dao/DbConnection.java
@@ -8,14 +8,17 @@ public class DbConnection {
     private static Connection firstInstance = setConnection();
 
     private static Connection setConnection() {
+        Connection connection = null;
         try {
             Class.forName("org.sqlite.JDBC");
-            return DriverManager.getConnection("jdbc:sqlite:resources/dbStruct.db");
+            connection = DriverManager.getConnection("jdbc:sqlite:resources/dbStruct.db");
         } catch (ClassNotFoundException | SQLException e) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
         }
+        return connection;
     }
+
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         return firstInstance;
     }
