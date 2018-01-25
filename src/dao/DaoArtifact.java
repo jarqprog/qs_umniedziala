7796a1592b920304efb0190199f18d8cb93c7069
Joanna Baran <joannabaran56@gmail.com>
2018-01-25 21:07:53 +0100

    DaoArtifact: add methods createArtifact

--- src/dao/DaoArtifact.java
+++ src/dao/DaoArtifact.java
@@ -1,9 +1,19 @@
 package dao;
 
 import model.*;
-import java.util.ArrayList;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DaoArtifact extends Dao{
 
+    public Artifact createArtifact(String name, int value, String description, String type) {
+        return new Artifact(name, value, description, type);
+    }
+
+    public Artifact createArtifact(int itemId, String name, int value, String description, String type) {
+        return new Artifact(itemId, name, value, description, type);
+    }
         
 }
