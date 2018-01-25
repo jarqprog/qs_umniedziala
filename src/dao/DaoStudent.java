c21fcee9ef21da774fa2dd634798b1e8e81dc080
martastaporek <staporekmarta1@gmail.com>
2018-01-25 18:51:21 +0100

    DaoStudent: Fixes to work with connection

--- src/dao/DaoStudent.java
+++ src/dao/DaoStudent.java
@@ -34,9 +34,9 @@ public class DaoStudent extends Dao {
             preparedStatement.close();
 
             student = createStudent(userId, name, password, email);
-            DaoWallet daoWallet = new DaoWallet();
-            Wallet wallet = daoWallet.importInstance(studentId); //metoda do napisania
-            student.setWallet(wallet);
+            //DaoWallet daoWallet = new DaoWallet();
+            //Wallet wallet = daoWallet.importInstance(studentId); //metoda do napisania
+            //student.setWallet(wallet);
 
         } catch (SQLException e) {
             return student;
