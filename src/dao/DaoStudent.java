e40c0d0dd321e743e7d3c4f50381b1b93ca196d6
Joanna Baran <joannabaran56@gmail.com>
2018-01-25 13:59:59 +0100

    DaoStudent: remove previous methods

--- src/dao/DaoStudent.java
+++ src/dao/DaoStudent.java
@@ -9,32 +9,7 @@ public class DaoStudent implements IDaoStudent{
         return new Student(userId, name, password, email);
     }
 
-    public Student getStudentById(int id){
-        for(Student student: students){
-            if(student.getUserId() == id){
-                return student;
-            }
-        }
-        return null;
-    }
 
-    public ArrayList <Student> getStudentsByClassId(int id){
-        ArrayList <Student> studentsInClass = new ArrayList<Student>();
-        for(Student student: students){
-            if(student.getClassId() == id){
-                studentsInClass.add(student);
-            }
-        }
-        return studentsInClass;
-    }
-
-    public void exportData(ArrayList <Student> updatedStudents){
-        students = updatedStudents;
-    }
-
-    public ArrayList <Student> importData(){
-        return students;
-    }
     private class DaoWallet{
 
         private Wallet wallet = new Wallet();
