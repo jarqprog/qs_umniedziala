f5759d14ebcc4c16631e53f4bc3c6f4e9023f3ee
filipHartman <filip.hartman23@gmail.com>
2018-01-25 14:52:10 +0100

    DbConnection: Add singleton class for connect to db

---
+++ src/dao/DbConnection.java
@@ -0,0 +1,58 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbConnection {
+    private static DbConnection firstInstance = null;
+    private Connection dbConnection;
+
+    private DbConnection() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            dbConnection = DriverManager.getConnection("jdbc:sqlite:dbStruct.db");
+            dbConnection.setAutoCommit(false);
+        }
+        catch (ClassNotFoundException | SQLException ex) {
+            System.err.println(ex.getClass().getName() + ": " + ex.getMessage());
+            System.exit(0);
+        }
+    }
+
+    public static DbConnection getInstance() {
+            if (firstInstance == null) {
+                firstInstance = new DbConnection();
+            }
+        return firstInstance;
+    }
+    public Statement createStatement() {
+        Statement stmt = null;
+        try {
+            stmt = dbConnection.createStatement();
+        } catch (SQLException ex) {
+            System.err.println(ex.getClass().getName() + ": " + ex.getMessage());
+            System.exit(0);
+        }
+        return stmt;
+    }
+
+    public void commit() {
+        try {
+            dbConnection.commit();
+        } catch (SQLException ex) {
+            System.err.println(ex.getClass().getName() + ": " + ex.getMessage());
+            System.exit(0);
+        }
+    }
+
+    public void close() {
+        try {
+            dbConnection.close();
+        } catch (SQLException ex) {
+            System.err.println(ex.getClass().getName() + ": " + ex.getMessage());
+            System.exit(0);
+        }
+    }
+}
