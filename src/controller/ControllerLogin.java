82180f9118cefe4039da1acd49932eaaa64b58cb
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-01-25 23:55:38 +0100

    ControllerLogin:Move setting db connection from login() to runMenu()

--- src/controller/ControllerLogin.java
+++ src/controller/ControllerLogin.java
@@ -22,43 +22,35 @@ import dao.DaoUser;  //testL
 
 public class ControllerLogin{
     private ViewLogin viewLogin = new ViewLogin();
-//    private DaoAdmin daoAdmin;
-//    private DaoMentor daoMentor;
-//    private DaoArtifact daoArtifact;
     private DaoStudent daoStudent;
-//    private DaoQuest daoQuest;
-//    private DaoClass daoClass;
     private DaoUser daoUser;
 
     public ControllerLogin(){
-//        this.daoAdmin = new DaoAdmin();
-//        this.daoMentor = new DaoMentor();
         this.daoStudent = new DaoStudent();
-//        this.daoArtifact = new DaoArtifact();
-//        this.daoQuest = new DaoQuest();
-//        this.daoClass = new DaoClass();
         this.daoUser = new DaoUser();
     }
 
     public void runMenu(){
+        if(daoUser.setConnection()){
 
-//        implementAllTestData();
+            String userOption = "";
+            while (!userOption.equals("0")) {
 
-        String userOption = "";
-        while (!userOption.equals("0")) {
-    
-            System.out.println("\nWhat would like to do?");
-            viewLogin.displayList(viewLogin.getLoginOptions());
-    
-            userOption = viewLogin.getInputFromUser("Option: ");
-            switch (userOption) {
-                case "1": login();
-                        break;
-                case "0": break;
-    
-                default: System.out.println("Wrong option. Try again!");
-                         break;
+                System.out.println("\nWhat would like to do?");
+                viewLogin.displayList(viewLogin.getLoginOptions());
+
+                userOption = viewLogin.getInputFromUser("Option: ");
+                switch (userOption) {
+                    case "1": login();
+                            break;
+                    case "0": break;
+
+                    default: System.out.println("Wrong option. Try again!");
+                             break;
+                }
             }
+
+        daoUser.closeConnection();
         }
     
     }
@@ -67,11 +59,7 @@ public class ControllerLogin{
         String userEmail = viewLogin.getInputFromUser("email: ");
         String userPassword = viewLogin.getInputFromUser("password: ");
 
-        User user = null;
-        if(daoUser.setConnection()){
-            user = daoUser.getUser(userEmail, userPassword);
-//            daoUser.closeConnection();
-        }
+        User user = daoUser.getUser(userEmail, userPassword);
 
         if(user != null){
             IUserController userController = getUserController(user);
@@ -83,31 +71,6 @@ public class ControllerLogin{
         }  
     }
 
-//    private ArrayList<User> getAllUsers(){
-//        ArrayList <User> users = new ArrayList<>();
-//
-//        users.addAll(this.daoAdmin.importData());
-//        users.addAll(this.daoMentor.importData());
-//        users.addAll(this.daoStudent.importData());
-//
-//        return users;
-//    }
-
-//    private User getUser(String email, String password){
-//        String query = "select * from 'users' where email='" + email + "' AND password='" + password + "';";
-//        User user = daoUser.executeQuery(query);
-//        ArrayList <User> users = getAllUsers();
-//        MyIterator <User> myIterator = new MyIterator<>(users);
-//
-//        while(myIterator.hasNext()){
-//            User user = myIterator.next();
-//            if(user.getEmail().equals(email) && user.getPassword().equals(password)){
-//                return user;
-//            }
-//        }
-//        return user;
-//    }
-//
     private IUserController getUserController(User user){
         IUserController controller = null;
 
@@ -122,12 +85,4 @@ public class ControllerLogin{
         return controller;
     }
 
-//    private void implementAllTestData(){
-//        this.daoAdmin.implementTestData();
-//        this.daoMentor.implementTestData();
-//        this.daoArtifact.implementTestData();
-//        this.daoStudent.implementTestData();
-//        this.daoQuest.implementTestData();
-//        this.daoClass.implementTestData();
-//    }
 }
