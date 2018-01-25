e9629eda3e97ed7f03263f9f09aa88d99b5305e4
Joanna Baran <joannabaran56@gmail.com>
2018-01-25 21:17:13 +0100

    DaoArtifact: add and implement method importInstance

--- src/dao/DaoArtifact.java
+++ src/dao/DaoArtifact.java
@@ -15,5 +15,32 @@ public class DaoArtifact extends Dao{
     public Artifact createArtifact(int itemId, String name, int value, String description, String type) {
         return new Artifact(itemId, name, value, description, type);
     }
+
+    private Artifact importInstance(int itemId) {
+        Artifact artifact = null;
+        PreparedStatement preparedStatement = null;
+        String query = "Select * from artifacts where id_artifact = ?";
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, itemId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                String name = resultSet.getString("name");
+                int value = resultSet.getInt("value");
+                String description = resultSet.getString("description");
+                String type = resultSet.getString("type");
+
+                artifact = new Artifact(itemId, name, value, description, type);
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+
+        return artifact;
+    }
         
 }
