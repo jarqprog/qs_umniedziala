4b4e98e67f958fa1942c6ae6c3fc1a08d29b7df5
filipHartman <filip.hartman23@gmail.com>
2018-01-25 18:16:57 +0100

    Dao: Create abstrcat dao

---
+++ src/dao/Dao.java
@@ -0,0 +1,29 @@
+package dao;
+
+
+import model.*;
+
+import java.sql.*;
+
+public class Dao {
+    private Connection connection;
+
+    public boolean setConnection() {
+        boolean isConnected;
+        try {
+             connection = DbConnection.getConnection();
+             isConnected = true;
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            isConnected = false;
+        }
+        return isConnected;
+    }
+    public void closeConnection(){
+        try{
+            connection.close();
+        }catch(SQLException e){
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+    }
+}
