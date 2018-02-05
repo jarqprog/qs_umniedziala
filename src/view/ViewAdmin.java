6aad97756f88bd5eb47b5d36a53b2dd01873fef4
martastaporek <staporekmarta1@gmail.com>
2018-02-05 15:23:54 +0100

    ViewAdmin: Add field editOptions

--- src/view/ViewAdmin.java
+++ src/view/ViewAdmin.java
@@ -11,6 +11,9 @@ public class ViewAdmin extends View{
     "2. Create class", "3. Edit mentor", "4. See mentor's profile","5. Assign mentor to class",
     "6. Create levels of experience", "0. Log out"));
 
+    private static ArrayList <String> editOptions = new ArrayList <String>(Arrays.asList("1. Edit mentor's e-mail",
+            "2. Reassign mentor to different class", "0. Go back" ));
+
     public ArrayList<String> getAdminOptions() {
         return adminOptions;
     }
