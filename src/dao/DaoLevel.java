38cd9c4f302bcf984bac796821e5a75fed947def
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-02-05 17:26:41 +0100

    DaoLevel:Add semicolon at the end of all return statements

--- src/dao/DaoLevel.java
+++ src/dao/DaoLevel.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import model.Level;
 
 public class DaoLevel{
-    public Level createLevel(String name, int coinsLimit){return null}
+    public Level createLevel(String name, int coinsLimit){return null;}
 
-    public Level createLevel(int levelId, String name, int coinsLimit){return null}
+    public Level createLevel(int levelId, String name, int coinsLimit){return null;}
 
     public void exportLevel(Level level){}
 
-    public Level importLevel(int levelId){return null}
+    public Level importLevel(int levelId){return null;}
 }
