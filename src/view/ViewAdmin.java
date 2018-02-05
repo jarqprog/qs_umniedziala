138e8f95d0ce9d0a6c9cfe4417b9111cb4832fa2
Joanna Baran <joannabaran56@gmail.com>
2018-02-05 11:48:01 +0100

    User views: change quit to log out in menus

--- src/view/ViewAdmin.java
+++ src/view/ViewAdmin.java
@@ -9,7 +9,7 @@ public class ViewAdmin extends View{
 
     private static ArrayList <String> adminOptions = new ArrayList <String>(Arrays.asList("1. Create mentor",
     "2. Create class", "3. Edit mentor", "4. See mentor's profile","5. Assign mentor to class",
-    "6. Create levels of experience", "0. Quit"));
+    "6. Create levels of experience", "0. Log out"));
 
     public ArrayList<String> getAdminOptions() {
         return adminOptions;
