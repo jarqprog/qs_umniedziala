1b09e065b228b59edf328142b7f2fbe0dd78fff7
Joanna Baran <joannabaran56@gmail.com>
2018-02-05 14:53:53 +0100

    DaoClass: add and implement exportClass

--- src/dao/DaoClass.java
+++ src/dao/DaoClass.java
@@ -1,9 +1,30 @@
 package dao;
 
 import java.util.ArrayList;
-import dao.DaoStudent;
-import model.*;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import model.CodecoolClass;
 
 public class DaoClass{
+
+    public void exportClass(CodecoolClass codecoolClass) {
+
+        String name = codecoolClass.getName();
+
+        PreparedStatement preparedStatement = null;
+        String query = "INSERT into codecool_classes (name) values (?);";
+
+        try {
+            preparedStatement = DbConnection.getConnection().prepareStatement(query);
+            preparedStatement.setString(1, name);
+
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Class insertion failed");
+        }
+
+    }
 
 }
