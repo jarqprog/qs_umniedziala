f31c4a7d08b46d0a39521a5d059807b431a4ac8e
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-02-05 17:24:54 +0100

    DaoLevel:Create class frame

--- src/dao/DaoLevel.java
+++ src/dao/DaoLevel.java
@@ -1,7 +1,14 @@
 package dao;
 
 import java.util.ArrayList;
-import model.*;
+import model.Level;
 
 public class DaoLevel{
+    public Level createLevel(String name, int coinsLimit){return null}
+
+    public Level createLevel(int levelId, String name, int coinsLimit){return null}
+
+    public void exportLevel(Level level){}
+
+    public Level importLevel(int levelId){return null}
 }
