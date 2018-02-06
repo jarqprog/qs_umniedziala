1ecb820f626a1db39601734cec93e8a4d642aeee
martastaporek <staporekmarta1@gmail.com>
2018-02-06 16:26:44 +0100

    DaoQuest: Implement exportQuest()

--- src/dao/DaoQuest.java
+++ src/dao/DaoQuest.java
@@ -1,6 +1,9 @@
 package dao;
 
 import model.*;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class DaoQuest{
@@ -13,6 +16,32 @@ public class DaoQuest{
         return new Quest(itemId,name, value, description, type, category);
     }
 
-    
-    
+    public void exportQuest(Quest quest){
+
+        String name = quest.getName();
+        int value = quest.getValue();
+        String description = quest.getDescription();
+        String type = quest.getType();
+        String category = quest.getCategory();
+
+        PreparedStatement preparedStatement = null;
+        String query = "INSERT into quests (name, value, description, type, category)" +
+                "values (?, ?, ?, ?, ?);";
+
+        try{
+            preparedStatement = DbConnection.getConnection().prepareStatement(query);
+            preparedStatement.setString(1, name);
+            preparedStatement.setInt(2, value);
+            preparedStatement.setString(3, description);
+            preparedStatement.setString(4, type);
+            preparedStatement.setString(5, category);
+
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+
+        }catch (SQLException | ClassNotFoundException e){
+            System.out.println("Quest insertion failed");
+        }
+
+    }
 }
