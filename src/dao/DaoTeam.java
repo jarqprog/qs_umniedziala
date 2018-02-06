a7dc81a6e1af106ede9f8fa26c3f70c6ccf74853
filipHartman <filip.hartman23@gmail.com>
2018-02-06 14:48:04 +0100

    DaoTeam: Add names of methods to implement

--- src/dao/DaoTeam.java
+++ src/dao/DaoTeam.java
@@ -4,6 +4,15 @@ import model.*;
 import java.util.ArrayList;
 
 public class DaoTeam{
+    public Team createTeam(String name) {
 
+    }
 
+    public Team createTeam (int groupId, String name, ArrayList<Student> students, int availableCoins) {
+
+    }
+
+    public void exportTeam(Team team) {
+        
+    }
 }
