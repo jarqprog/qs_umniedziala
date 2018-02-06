f2a857a38211ca98058a22ae611c5f06b84df721
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-02-06 21:38:11 +0100

    ViewMentor:Add updateArtifactsOptions and updateArtifactTypeOptions fields; Add and fill getUpdateArtifactsOptions() and getUpdateArtifactTypeOptions()

--- src/view/ViewMentor.java
+++ src/view/ViewMentor.java
@@ -14,6 +14,13 @@ public class ViewMentor extends View{
     private static ArrayList <String> artifactEditOptions = new ArrayList<>(Arrays.asList("1. Change name",
             "2. Change value", "3. Change description", "4. Change type", "5. Change category"));
 
+    private static ArrayList <String> updateArtifactsOptions = new ArrayList <String>(Arrays.asList("1. Update artifacts name",
+            "2. Update artifacts description", "3. Update artifacts value", "4. Update artifacts type",
+            "0. Log out"));
+
+    private static ArrayList <String> updateArtifactTypeOptions = new ArrayList <String>(Arrays.asList("1. Type individual",
+            "2. Type team", "0. Log out"));
+
     public ArrayList<String> getMentorOptions() {
         return mentorOptions;
     }
@@ -21,4 +28,14 @@ public class ViewMentor extends View{
     public static ArrayList<String> getArtifactEditOptions() {
         return artifactEditOptions;
     }
+
+    public ArrayList<String> getUpdateArtifactsOptions() {
+        return updateArtifactsOptions;
+    }
+
+    public ArrayList<String> getUpdateArtifactTypeOptions() {
+        return updateArtifactTypeOptions;
+    }
+
+
 }
