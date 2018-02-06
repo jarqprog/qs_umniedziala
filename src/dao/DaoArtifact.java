7db062379abf1b46cae53f47574e874ffe93e12a
Joanna Baran <joannabaran56@gmail.com>
2018-02-06 19:04:34 +0100

    DaoArtifact: add and implement getAllArtifacts

--- src/dao/DaoArtifact.java
+++ src/dao/DaoArtifact.java
@@ -2,9 +2,11 @@ package dao;
 
 import model.*;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class DaoArtifact{
 
@@ -41,6 +43,34 @@ public class DaoArtifact{
         }
 
         return artifact;
+    }
+
+    public ArrayList<Artifact> getAllArtifacts() {
+        ArrayList<Artifact> artifacts = new ArrayList<>();
+        String query = "SELECT id_artifact FROM artifacts;";
+
+        try {
+            Connection connection = DbConnection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()){
+                int artifactId = resultSet.getInt("id_artifact");
+                Artifact artifact = importArtifact(artifactId);
+                artifacts.add(artifact);
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+
+        for (Artifact artifact : artifacts) {
+            System.out.println(artifact);
+        }
+        return artifacts;
     }
         
 }
