6f9511e76fe1008d033799e807cc1eba81e03e20
filipHartman <filip.hartman23@gmail.com>
2018-02-06 14:51:25 +0100

    DaoTeam: Implemet second method to create team model

--- src/dao/DaoTeam.java
+++ src/dao/DaoTeam.java
@@ -9,7 +9,7 @@ public class DaoTeam{
     }
 
     public Team createTeam (int groupId, String name, ArrayList<Student> students, int availableCoins) {
-
+        return new Team(groupId, name, students, availableCoins);
     }
 
     public void exportTeam(Team team) {
