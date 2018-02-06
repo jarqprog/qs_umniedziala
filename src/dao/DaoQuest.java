b4fcbe828711e4d3e44aac747805fd8db6428969
filipHartman <filip.hartman23@gmail.com>
2018-02-06 15:51:56 +0100

    DaoQuest: Add creating quest from data from database

--- src/dao/DaoQuest.java
+++ src/dao/DaoQuest.java
@@ -4,7 +4,8 @@ import model.*;
 import java.util.ArrayList;
 
 public class DaoQuest{
+    public Quest createQuest(int itemId, String name, int value, String description, String type, String category) {
+        return new Quest(itemId, name, value, description, type, category);
+    }
 
-
-    
 }
