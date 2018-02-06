04b88e0175d80dc702b676f4ca0aa32eb95d4b0d
martastaporek <staporekmarta1@gmail.com>
2018-02-06 16:12:50 +0100

    DaoQuest: Implement createQuest

--- src/dao/DaoQuest.java
+++ src/dao/DaoQuest.java
@@ -5,6 +5,14 @@ import java.util.ArrayList;
 
 public class DaoQuest{
 
+    public Quest createQuest(String name, int value, String description, String type, String category){
+        return new Quest(name, value, description, type, category);
+    }
 
+    public Quest createQuest(int itemId, String name, int value, String description, String type, String category){
+        return new Quest(itemId,name, value, description, type, category);
+    }
+
+    
     
 }
