11e6ff0aaeea57ae2f83c4a98df5624bff877e3b
martastaporek <staporekmarta1@gmail.com>
2018-02-06 18:21:28 +0100

    ViewMentor: Add artifactEditOptions field

--- src/view/ViewMentor.java
+++ src/view/ViewMentor.java
@@ -11,8 +11,14 @@ public class ViewMentor extends View{
         "6. Edit artifact", "7. Mark quest", "8. Mark artifact", "9. See student's wallet",
         "0. Log out"));
 
+    private static ArrayList <String> artifactEditOptions = new ArrayList<>(Arrays.asList("1. Change name",
+            "2. Change value", "3. Change description", "4. Change type", "5. Change category"));
+
     public ArrayList<String> getMentorOptions() {
         return mentorOptions;
     }
 
+    public static ArrayList<String> getArtifactEditOptions() {
+        return artifactEditOptions;
+    }
 }
