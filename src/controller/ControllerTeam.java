3915653516ac9635950eb0edd58034eb8887fb35
martastaporek <staporekmarta1@gmail.com>
2018-02-07 17:34:22 +0100

    ControllerTeam: Add constructor

--- src/controller/ControllerTeam.java
+++ src/controller/ControllerTeam.java
@@ -1,5 +1,32 @@
 package controller;
 
-public class ControllerTeam{
-    ;
+import view.ViewTeam;
+import model.*;
+
+public class ControllerTeam implements IUserController{
+
+    private ViewTeam viewTeam;
+    private Team team;
+
+    public ControllerTeam(Team team){
+        this.viewTeam = new ViewTeam();
+        this.team = team;
+    }
+
+
+    public void runMenu(){
+
+        String teamOption = "";
+        while (!teamOption.equals("0")) {
+
+            viewTeam.displayText("\nWhat would like to do?");
+            viewTeam.displayList(viewTeam.getStudentOptions());
+
+            teamOption = viewTeam.getInputFromUser("Option: ");
+            switch (teamOption) {
+                case "1": seeWallet();
+                    break;
+                case "2": buyArtifact();
+
+    }
 }
