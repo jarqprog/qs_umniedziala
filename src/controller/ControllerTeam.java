f963c67bb6b838909db2c47dff06c57b515900c8
Joanna Baran <joannabaran56@gmail.com>
2018-02-07 22:43:45 +0100

    ControllerTeam: add and implement popRandomStudent()

--- src/controller/ControllerTeam.java
+++ src/controller/ControllerTeam.java
@@ -3,11 +3,19 @@ package controller;
 import model.Student;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ControllerTeam{
 
     private boolean checkTeamCoinsDivisibleByTeamSize(int teamCoins, int teamSize) {
         return (teamCoins % teamSize) == 0;
+    }
+
+    private Student popRandomStudent(ArrayList<Student> students) {
+        int randomIndex = new Random.nextInt(students.size());
+        Student randomStudent = students.get(randomIndex);
+        students.remove(randomStudent);
+        return randomStudent;
     }
 
     private void splitMoneyEqually() {
