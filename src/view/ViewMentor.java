e86c32ad438c4629176bc40f3ae71e68db54c4d0
filipHartman <filip.hartman23@gmail.com>
2018-02-07 22:08:30 +0100

    ViewMentor: Add list of option to change type of quest

--- src/view/ViewMentor.java
+++ src/view/ViewMentor.java
@@ -14,10 +14,12 @@ public class ViewMentor extends View{
     private static ArrayList <String> questEditOptions = new ArrayList<>(Arrays.asList("1. Change name",
             "2. Change value", "3. Change description", "4. Change type", "5. Change category"));
 
-    private static ArrayList <String> updateQuestsOptions = new ArrayList <String>(Arrays.asList("1. Update quest's name",
+    private static ArrayList <String> updateQuestOptions = new ArrayList <String>(Arrays.asList("1. Update quest's name",
             "2. Update quest's description", "3. Update quest's value", "4. Update quest's type", "5. Update quest's category",
             "0. Go back"));
 
+    private static ArrayList <String> updateQuestTypeOptions = new ArrayList <String>(Arrays.asList("1. Type individual",
+            "2. Type team", "0. Go back"));
     public ArrayList<String> getMentorOptions() {
         return mentorOptions;
     }
