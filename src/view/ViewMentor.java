c27e8bc297af5d976603767ab8c02b589610a533
filipHartman <filip.hartman23@gmail.com>
2018-02-07 22:06:51 +0100

    ViewMentor: Add list of options for update quests

--- src/view/ViewMentor.java
+++ src/view/ViewMentor.java
@@ -11,6 +11,13 @@ public class ViewMentor extends View{
         "6. Edit artifact", "7. Mark quest", "8. Mark artifact", "9. See student's wallet",
         "0. Log out"));
 
+    private static ArrayList <String> questEditOptions = new ArrayList<>(Arrays.asList("1. Change name",
+            "2. Change value", "3. Change description", "4. Change type", "5. Change category"));
+
+    private static ArrayList <String> updateQuestsOptions = new ArrayList <String>(Arrays.asList("1. Update quest's name",
+            "2. Update quest's description", "3. Update quest's value", "4. Update quest's type", "5. Update quest's category",
+            "0. Go back"));
+
     public ArrayList<String> getMentorOptions() {
         return mentorOptions;
     }
