fef93e6db3170d68eceedafe9bf678b55322015a
martastaporek <staporekmarta1@gmail.com>
2018-02-07 17:28:21 +0100

    ViewStudent: Add option in field studentOptions

--- src/view/ViewStudent.java
+++ src/view/ViewStudent.java
@@ -9,7 +9,7 @@ public class ViewStudent extends View{
 
     private static ArrayList <String> studentOptions = new ArrayList <String>(Arrays.asList("1. See wallet details",
         "2. Buy artifact", "3. Check experience level", "4. Manage team", "5. See available quests",
-        "6. See available artifacts", "0. Log out"));
+        "6. See available artifacts", "7. Manage team", "0. Log out"));
 
     public ArrayList<String> getStudentOptions() {
         return studentOptions;
