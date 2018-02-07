6a14f42625e1ef473730042ddd13c5373d37991a
Joanna Baran <joannabaran56@gmail.com>
2018-02-07 21:40:36 +0100

    ControllerTeam: add checkTeamCoinsDivisibleByTeamSize

--- src/controller/ControllerTeam.java
+++ src/controller/ControllerTeam.java
@@ -1,5 +1,13 @@
 package controller;
 
+import model.Student;
+
+import java.util.ArrayList;
+
 public class ControllerTeam{
-    ;
+
+    public boolean checkTeamCoinsDivisibleByTeamSize(int teamCoins, int teamSize) {
+        return ((double) teamCoins % teamSize) == 0;
+    }
+
 }
