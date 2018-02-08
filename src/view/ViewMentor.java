3faf458dcaff683c2f97fc215b9313048dfb37d3
Joanna Baran <joannabaran56@gmail.com>
2018-02-08 19:52:46 +0100

    ViewMentor: fix typo in mentor options

--- src/view/ViewMentor.java
+++ src/view/ViewMentor.java
@@ -8,7 +8,7 @@ public class ViewMentor extends View{
 
     private static ArrayList <String> mentorOptions = new ArrayList <String>(Arrays.asList("1. Create student",
         "2. Create team", "3. Create quest", "4. Add artifact to store", "5. Edit quest",
-        "6. Edit artifact", "7. Mark quest", "8. Mark artifact", "9. See student's wallet",
+        "6. Edit artifact", "7. Mark quest", "8. Mark artifact", "9. See students' wallet",
         "10. Assign student to team", "0. Go back"));
 
     private static ArrayList <String> assignStudentToTeamOptions = new ArrayList <String>(Arrays.asList("1. Assign student to chosen team", "0. Go back"));
