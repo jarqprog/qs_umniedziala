e3c2eea5b4824d21d8b44b781615a6a5c9b07f93
Joanna Baran <joannabaran56@gmail.com>
2018-03-06 23:08:10 +0100

    DaoClass: add try-with-resources in methods accessing db

--- src/dao/DaoClass.java
+++ src/dao/DaoClass.java
@@ -1,5 +1,6 @@
 package dao;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.sql.PreparedStatement;
@@ -20,23 +21,20 @@ public class DaoClass{
 
     public CodecoolClass importClass(Integer classID){
         CodecoolClass codecoolClass = null;
-        PreparedStatement preparedStatement;
-
         String query = "SELECT * from codecool_classes where id_codecool_class = ?;";
 
-        try {
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
             preparedStatement.setInt(1, classID);
-            ResultSet resultSet = preparedStatement.executeQuery();
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
 
-            if(!resultSet.isClosed()) {
-                String name = resultSet.getString("name");
-                ArrayList<Student> students = getStudentsOfClass(classID);
-
-                codecoolClass = createClass(classID, name, students);
-                resultSet.close();
+                if (!resultSet.isClosed()) {
+                    String name = resultSet.getString("name");
+                    ArrayList<Student> students = getStudentsOfClass(classID);
+                    codecoolClass = createClass(classID, name, students);
+                }
             }
-            preparedStatement.close();
 
         }catch (SQLException  e){
             System.out.println("Class not found");
@@ -46,18 +44,14 @@ public class DaoClass{
     }
 
     public boolean exportClass(CodecoolClass codecoolClass) {
-
         String name = codecoolClass.getName();
-
-        PreparedStatement preparedStatement = null;
         String query = "INSERT into codecool_classes (name) values (?);";
 
-        try {
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
-            preparedStatement.setString(1, name);
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
 
+            preparedStatement.setString(1, name);
             preparedStatement.executeUpdate();
-            preparedStatement.close();
             return true;
 
         } catch (SQLException  e) {
@@ -67,14 +61,12 @@ public class DaoClass{
 
     public ArrayList<CodecoolClass> getAllClasses(){
         ArrayList <CodecoolClass> allCodecoolClasses = new ArrayList <CodecoolClass> ();
-
-        PreparedStatement preparedStatement = null;
         String query = "SELECT * FROM codecool_classes;";
         CodecoolClass codecoolClass;
 
-        try {
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
-            ResultSet resultSet = preparedStatement.executeQuery();
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
 
             while(resultSet.next()) {
                 int classId = resultSet.getInt("id_codecool_class");
@@ -83,10 +75,7 @@ public class DaoClass{
 
                 codecoolClass = createClass(classId, name, students);
                 allCodecoolClasses.add(codecoolClass);
-
             }
-            resultSet.close();
-            preparedStatement.close();
 
         } catch (SQLException  e) {
             System.out.println("No Classes");
@@ -95,16 +84,14 @@ public class DaoClass{
     }
 
     public void assignMentorToClass(Integer mentorId, Integer classId){
-        PreparedStatement preparedStatement = null;
         String query = "INSERT into mentors_in_classes (id_codecool_class, id_mentor) values (?, ?);";
 
-        try {
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
             preparedStatement.setInt(1, classId);
             preparedStatement.setInt(2, mentorId);
-
             preparedStatement.executeUpdate();
-            preparedStatement.close();
 
         } catch (SQLException  e) {
             System.out.println("Assigning mentor to class failed");
@@ -112,16 +99,14 @@ public class DaoClass{
     }
 
     public void assignStudentToClass(Integer studentId, Integer classId){
-        PreparedStatement preparedStatement = null;
         String query = "INSERT into students_in_classes (id_codecool_class, id_student) values (?, ?);";
 
-        try {
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
             preparedStatement.setInt(1, classId);
             preparedStatement.setInt(2, studentId);
-
             preparedStatement.executeUpdate();
-            preparedStatement.close();
 
         } catch (SQLException  e) {
             System.out.println("Assigning student to class failed");
@@ -129,16 +114,14 @@ public class DaoClass{
     }
 
     public void updateMentorInClass(Integer mentorId, Integer classId){
-        PreparedStatement preparedStatement = null;
         String query = "UPDATE mentors_in_classes SET id_codecool_class=? WHERE id_mentor=?;";
 
-        try {
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
             preparedStatement.setInt(1, classId);
             preparedStatement.setInt(2, mentorId);
-
             preparedStatement.executeUpdate();
-            preparedStatement.close();
 
         } catch (SQLException  e) {
             System.out.println("updating mentors class assignment failed");
@@ -146,15 +129,13 @@ public class DaoClass{
     }
 
     public void unsignMentorFromClass(Integer mentorId){
-        PreparedStatement preparedStatement = null;
         String query = "DELETE FROM mentors_in_classes WHERE id_mentor=?;";
 
-        try {
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
-            preparedStatement.setInt(1, mentorId);
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
 
+            preparedStatement.setInt(1, mentorId);
             preparedStatement.executeUpdate();
-            preparedStatement.close();
 
         } catch (SQLException  e) {
             System.out.println("Unsigning mentor from class failed");
@@ -163,23 +144,19 @@ public class DaoClass{
 
     public ArrayList<Student> getStudentsOfClass(Integer classID){
         ArrayList <Student> studentsInClass = new ArrayList <> ();
-
-        PreparedStatement preparedStatement = null;
         String query = "SELECT id_user FROM users JOIN students_in_classes WHERE students_in_classes.id_codecool_class=? AND students_in_classes.id_student=users.id_user;";
 
-        try {
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
             preparedStatement.setInt(1, classID);
-            ResultSet resultSet = preparedStatement.executeQuery();
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
 
-            while(resultSet.next()) {
-                int userId = resultSet.getInt("id_user");
-
-                studentsInClass.add(new DaoStudent().importInstance(userId));
-
+                while (resultSet.next()) {
+                    int userId = resultSet.getInt("id_user");
+                    studentsInClass.add(new DaoStudent().importInstance(userId));
+                }
             }
-            resultSet.close();
-            preparedStatement.close();
 
         } catch (SQLException  e) {
             System.out.println("No students");
@@ -188,23 +165,20 @@ public class DaoClass{
     }
 
     public CodecoolClass getMentorsClass(Integer mentorId){
-        PreparedStatement preparedStatement;
         CodecoolClass mentorsClass = null;
-
         String query = "SELECT id_codecool_class FROM mentors_in_classes WHERE id_mentor=?;";
 
-        try {
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
             preparedStatement.setInt(1, mentorId);
-            ResultSet resultSet = preparedStatement.executeQuery();
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
 
-            if(!resultSet.isClosed()){
-                Integer classId = resultSet.getInt("id_codecool_class");
-
-                mentorsClass = importClass(classId);
-                resultSet.close();
+                if (!resultSet.isClosed()) {
+                    Integer classId = resultSet.getInt("id_codecool_class");
+                    mentorsClass = importClass(classId);
+                }
             }
-            preparedStatement.close();
 
         } catch (SQLException  e) {
             System.out.println("updating mentors class assignment failed");
