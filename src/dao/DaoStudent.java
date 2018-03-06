4642baac6b29810cda54793977aa922b7387f0d1
Joanna Baran <joannabaran56@gmail.com>
2018-03-06 23:58:56 +0100

    DaoStudent: add try-with-resources in methods accessing db

--- src/dao/DaoStudent.java
+++ src/dao/DaoStudent.java
@@ -19,32 +19,27 @@ public class DaoStudent implements IDaoUser <Student>{
 
     public Student importInstance(int studentId) {
         Student student = null;
-        PreparedStatement preparedStatement = null;
         int roleId = getRoleID("student");
-
         String query = "SELECT * FROM users WHERE id_user = ? AND id_role = ?;";
 
-        try {
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
             preparedStatement.setInt(1, studentId);
             preparedStatement.setInt(2, roleId);
-            ResultSet resultSet = preparedStatement.executeQuery();
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
 
-            if(!resultSet.isClosed()){
+                if (!resultSet.isClosed()) {
+                    int userId = resultSet.getInt("id_user");
+                    String name = resultSet.getString("name");
+                    String password = resultSet.getString("password");
+                    String email = resultSet.getString("email");
 
-                int userId = resultSet.getInt("id_user");
-
-                String name = resultSet.getString("name");
-                String password = resultSet.getString("password");
-                String email = resultSet.getString("email");
-
-                student = createInstance(userId, name, password, email);
-                Wallet wallet = new DaoWallet().importWallet(studentId);
-                student.setWallet(wallet);
-
-                resultSet.close();
+                    student = createInstance(userId, name, password, email);
+                    Wallet wallet = new DaoWallet().importWallet(studentId);
+                    student.setWallet(wallet);
+                }
             }
-            preparedStatement.close();
 
         } catch (SQLException  e) {
             System.out.println(e.getMessage());
@@ -55,32 +50,26 @@ public class DaoStudent implements IDaoUser <Student>{
     }
 
     public Student importNewStudent(String userEmail){
-
         Student student = null;
-        PreparedStatement preparedStatement = null;
         int roleId = getRoleID("student");
-
         String query = "SELECT * FROM users WHERE email = ? AND id_role = ?;";
 
-        try {
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
             preparedStatement.setString(1, userEmail);
             preparedStatement.setInt(2, roleId);
-            ResultSet resultSet = preparedStatement.executeQuery();
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
 
-            if(!resultSet.isClosed()){
+                if (!resultSet.isClosed()) {
+                    int userId = resultSet.getInt("id_user");
+                    String name = resultSet.getString("name");
+                    String password = resultSet.getString("password");
+                    String email = resultSet.getString("email");
 
-                int userId = resultSet.getInt("id_user");
-
-                String name = resultSet.getString("name");
-                String password = resultSet.getString("password");
-                String email = resultSet.getString("email");
-
-                student = createInstance(userId, name, password, email);
-
-                resultSet.close();
+                    student = createInstance(userId, name, password, email);
+                }
             }
-            preparedStatement.close();
 
         } catch (SQLException  e) {
             System.out.println(e.getMessage());
@@ -92,25 +81,23 @@ public class DaoStudent implements IDaoUser <Student>{
 
 
     public boolean exportInstance(Student student) {
-
         String name = student.getName();
         String password = student.getPassword();
         String email = student.getEmail();
         int roleId = getRoleID("student");
 
-        PreparedStatement preparedStatement = null;
         String query = "INSERT into users (name, password, email, id_role)" +
                 "values (?, ?, ?, ?);";
 
-        try {
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, password);
             preparedStatement.setString(3, email);
             preparedStatement.setInt(4, roleId);
 
             preparedStatement.executeUpdate();
-            preparedStatement.close();
             return true;
 
         } catch (SQLException  e) {
@@ -125,12 +112,10 @@ public class DaoStudent implements IDaoUser <Student>{
         int studentId = student.getUserId();
         int roleId = getRoleID("student");
 
-
-        PreparedStatement preparedStatement = null;
         String query = "update users set name = ?, password = ?, email = ? where id_user= ? AND id_role = ?;";
 
-        try {
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
 
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, password);
@@ -139,7 +124,6 @@ public class DaoStudent implements IDaoUser <Student>{
             preparedStatement.setInt(5, roleId);
 
             preparedStatement.executeUpdate();
-            preparedStatement.close();
             return true;
 
         } catch (SQLException  e) {
@@ -148,22 +132,19 @@ public class DaoStudent implements IDaoUser <Student>{
     }
 
     public int getRoleID(String roleName){
-
         int roleId = 0;
-        PreparedStatement preparedStatement = null;
-
         String query = "SELECT id_role from roles where name = ?;";
 
-        try {
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
-            preparedStatement.setString(1, roleName);
-            ResultSet resultSet = preparedStatement.executeQuery();
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
 
-            if(!resultSet.isClosed()) {
-                roleId = resultSet.getInt("id_role");
-                resultSet.close();
+            preparedStatement.setString(1, roleName);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+
+                if (!resultSet.isClosed()) {
+                    roleId = resultSet.getInt("id_role");
+                }
             }
-            preparedStatement.close();
 
         }catch (SQLException  e){
             System.out.println("Role not found");
@@ -178,20 +159,18 @@ public class DaoStudent implements IDaoUser <Student>{
         int roleId = getRoleID("student");
         String query = "SELECT id_user FROM users WHERE id_role = ?;";
 
-        try {
-            Connection connection = DbConnection.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
             preparedStatement.setInt(1, roleId);
-            ResultSet resultSet = preparedStatement.executeQuery();
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
 
-            while (resultSet.next()){
-                int userId = resultSet.getInt("id_user");
-                Student student = importInstance(userId);
-                students.add(student);
+                while (resultSet.next()) {
+                    int userId = resultSet.getInt("id_user");
+                    Student student = importInstance(userId);
+                    students.add(student);
+                }
             }
-
-            resultSet.close();
-            preparedStatement.close();
 
         } catch (SQLException  e) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
