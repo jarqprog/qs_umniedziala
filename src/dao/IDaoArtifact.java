3b7b65c31ac1d300b17418cff0ac097074eb47f1
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-03-06 20:06:08 +0100

    IDaoArtifact: Switch List type to ArrayList to maintain backward compatibility

--- src/dao/IDaoArtifact.java
+++ src/dao/IDaoArtifact.java
@@ -1,15 +1,16 @@
 package dao;
 
 import model.Artifact;
-import java.util.List;
+
+import java.util.ArrayList;
 
 public interface IDaoArtifact {
     Artifact createArtifact(String name, int value, String description, String type);
     Artifact createArtifact(int itemId, String name, int value, String description, String type);
     Artifact importArtifact(int itemId);
-    List<Artifact> getAllArtifacts();
+    ArrayList<Artifact> getAllArtifacts();
     boolean updateArtifact(Artifact artifact);
     boolean exportArtifact(Artifact artifact);
-    List<Artifact> getArtifacts(String type);
+    ArrayList<Artifact> getArtifacts(String type);
 
     }
