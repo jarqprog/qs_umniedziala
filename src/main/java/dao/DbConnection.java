228881c250f16e824639e0907f810e8b3ff1864c
martastaporek <staporekmarta1@gmail.com>
2018-03-08 16:53:33 +0100

    DbConnection: Remove sigleton pattern

--- src/main/java/dao/DbConnection.java
+++ src/main/java/dao/DbConnection.java
@@ -5,21 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbConnection {
-    private static Connection firstInstance = setConnection();
 
-    private static Connection setConnection() {
+    public static Connection getConnection() {
         Connection connection = null;
         try {
             Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/dbStruct.db");
+            connection = DriverManager.getConnection("jdbc:sqlite:resources/dbStruct.db");
         } catch (ClassNotFoundException | SQLException e) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
         }
         return connection;
+
     }
 
-    public static Connection getConnection() {
-        return firstInstance;
-    }
 }
