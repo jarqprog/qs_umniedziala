deb79e8e00d781c7c1fc38929f85422031211676
martastaporek <staporekmarta1@gmail.com>
2018-03-08 17:49:51 +0100

    DaoMentor: Fix to work with ARM

--- src/main/java/dao/DaoMentor.java
+++ src/main/java/dao/DaoMentor.java
@@ -5,6 +5,7 @@ import model.*;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,27 +24,26 @@ public class DaoMentor implements IDaoMentor {
     @Override
     public Mentor importMentor(int mentorId) {
         Mentor mentor = null;
-        PreparedStatement preparedStatement = null;
+
         int roleId = getRoleID("mentor");
         String query = "SELECT * FROM users WHERE id_user = ? AND id_role = ?;";
 
-        try {
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
-            preparedStatement.setInt(1, mentorId);
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+             preparedStatement.setInt(1, mentorId);
             preparedStatement.setInt(2, roleId);
-            ResultSet resultSet = preparedStatement.executeQuery();
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
 
-            if(!resultSet.isClosed()) {
-                int userId = resultSet.getInt("id_user");
-                String name = resultSet.getString("name");
-                String password = resultSet.getString("password");
-                String email = resultSet.getString("email");
+                if (!resultSet.isClosed()) {
+                    int userId = resultSet.getInt("id_user");
+                    String name = resultSet.getString("name");
+                    String password = resultSet.getString("password");
+                    String email = resultSet.getString("email");
 
-                mentor = createMentor(userId, name, password, email);
-
-                resultSet.close();
+                    mentor = createMentor(userId, name, password, email);
+                }
             }
-            preparedStatement.close();
 
         } catch (SQLException e) {
             return mentor;
@@ -59,19 +59,19 @@ public class DaoMentor implements IDaoMentor {
         String email = mentor.getEmail();
         int roleId = getRoleID("mentor");
 
-        PreparedStatement preparedStatement = null;
+
         String query = "INSERT INTO users (name, password, email, id_role)" +
                        "VALUES (?, ?, ?, ?);";
 
-        try{
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, password);
             preparedStatement.setString(3, email);
             preparedStatement.setInt(4, roleId);
 
             preparedStatement.executeUpdate();
-            preparedStatement.close();
             return true;
 
         }catch (SQLException e){
@@ -87,21 +87,20 @@ public class DaoMentor implements IDaoMentor {
         int mentorId = mentor.getUserId();
         int roleId = getRoleID("mentor");
 
-
-        PreparedStatement preparedStatement = null;
         String query = "UPDATE users SET name = ?, password = ?, email = ? "+
                 "WHERE id_user= ? AND id_role = ?;";
 
-        try{
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, password);
             preparedStatement.setString(3, email);
             preparedStatement.setInt(4, mentorId);
             preparedStatement.setInt(5, roleId);
             preparedStatement.executeUpdate();
-            preparedStatement.close();
             return true;
+
         } catch (SQLException e){
             return false;
         }
@@ -110,20 +109,19 @@ public class DaoMentor implements IDaoMentor {
     public int getRoleID(String roleName){
 
         int roleId = 0;
-        PreparedStatement preparedStatement = null;
+
 
         String query = "SELECT id_role FROM roles WHERE name = ?;";
 
-        try {
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
-            preparedStatement.setString(1, roleName);
-            ResultSet resultSet = preparedStatement.executeQuery();
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+             preparedStatement.setString(1, roleName);
+            try(ResultSet resultSet = preparedStatement.executeQuery()) {
 
-            if(!resultSet.isClosed()) {
-                roleId = resultSet.getInt("id_role");
-                resultSet.close();
+                if (!resultSet.isClosed()) {
+                    roleId = resultSet.getInt("id_role");
+                }
             }
-            preparedStatement.close();
 
         }catch (SQLException e){
             System.out.println("Role not found");
@@ -136,20 +134,19 @@ public class DaoMentor implements IDaoMentor {
     @Override
     public Integer getMentorClassId(Mentor mentor){
         Integer classId = null;
-        PreparedStatement preparedStatement;
+
 
         String query = "SELECT id_codecool_class FROM mentors_in_classes WHERE id_mentor = ?;";
 
-        try {
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
             preparedStatement.setInt(1, mentor.getUserId());
             ResultSet resultSet = preparedStatement.executeQuery();
 
             if(!resultSet.isClosed()) {
                 classId = resultSet.getInt("id_codecool_class");
-                resultSet.close();
             }
-            preparedStatement.close();
 
         }catch (SQLException e){
             System.out.println("Class not found");
@@ -164,27 +161,25 @@ public class DaoMentor implements IDaoMentor {
         List <Mentor> mentorList = new ArrayList <Mentor> ();
         int roleId = getRoleID("mentor");
 
-        PreparedStatement preparedStatement = null;
+
         String query = "SELECT * FROM users WHERE id_role = ?;";
         Mentor mentor;
 
-        try {
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
-            preparedStatement.setInt(1, roleId);
-            ResultSet resultSet = preparedStatement.executeQuery();
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+             preparedStatement.setInt(1, roleId);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
 
-            while(resultSet.next()) {
-                int userId = resultSet.getInt("id_user");
-                String name = resultSet.getString("name");
-                String password = resultSet.getString("password");
-                String email = resultSet.getString("email");
+                while (resultSet.next()) {
+                    int userId = resultSet.getInt("id_user");
+                    String name = resultSet.getString("name");
+                    String password = resultSet.getString("password");
+                    String email = resultSet.getString("email");
 
-                mentor = createMentor(userId, name, password, email);
-                mentorList.add(mentor);
-
+                    mentor = createMentor(userId, name, password, email);
+                    mentorList.add(mentor);
+                }
             }
-            resultSet.close();
-            preparedStatement.close();
 
         } catch (SQLException e) {
             System.out.println("No mentors");
