1b17d9cb881aea0e58704bc7916ac68363731621
martastaporek <staporekmarta1@gmail.com>
2018-03-08 17:03:19 +0100

    DaoAdmin: Fix to work with ARM

--- src/main/java/dao/DaoAdmin.java
+++ src/main/java/dao/DaoAdmin.java
@@ -22,13 +22,13 @@ public class DaoAdmin implements IDaoAdmin  {
     @Override
     public Admin importAdmin(int adminId){
         Admin admin = null;
-        PreparedStatement preparedStatement = null;
         String query = "SELECT * FROM users WHERE id_user = ? AND id_role = ?;";
         int roleId = getRoleID("admin");
 
 
-        try {
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
+        try (Connection connection = DbConnection.getConnection();
+                 PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            
             preparedStatement.setInt(1, adminId);
             preparedStatement.setInt(2, roleId);
             ResultSet resultSet = preparedStatement.executeQuery();
@@ -40,10 +40,8 @@ public class DaoAdmin implements IDaoAdmin  {
                 String email = resultSet.getString("email");
 
                 admin = createAdmin(userId, name, password, email);
-
-                resultSet.close();
             }
-            preparedStatement.close();
+            
 
         } catch (SQLException e) {
             return admin;
@@ -57,18 +55,19 @@ public class DaoAdmin implements IDaoAdmin  {
         String email = admin.getEmail();
         int roleId = getRoleID("admin");
 
-        PreparedStatement preparedStatement = null;
+        
         String query = "INSERT INTO users (name, password, email, id_role)" +
                 "VALUES (?, ?, ?, ?);";
 
-        try{
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, password);
             preparedStatement.setString(3, email);
             preparedStatement.setInt(4, roleId);
             preparedStatement.executeUpdate();
-            preparedStatement.close();
+            
             return true;
 
         }catch (SQLException e){
@@ -84,20 +83,20 @@ public class DaoAdmin implements IDaoAdmin  {
         int adminId = admin.getUserId();
         int roleId = getRoleID("admin");
 
-        PreparedStatement preparedStatement = null;
+        
         String query = "UPDATE users SET name = ?, password = ?, email = ?"+
                 "WHERE id_user= ? AND id_role = ?;";
 
-        try{
-
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, password);
             preparedStatement.setString(3, email);
             preparedStatement.setInt(4, adminId);
             preparedStatement.setInt(5, roleId);
             preparedStatement.executeUpdate();
-            preparedStatement.close();
+            
             return true;
         } catch (SQLException e){
             return false;
@@ -107,20 +106,19 @@ public class DaoAdmin implements IDaoAdmin  {
     public int getRoleID(String roleName){
 
         int roleId = 0;
-        PreparedStatement preparedStatement = null;
+        
 
         String query = "SELECT id_role FROM roles WHERE name = ?;";
 
-        try {
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)){
             preparedStatement.setString(1, roleName);
             ResultSet resultSet = preparedStatement.executeQuery();
 
             if(!resultSet.isClosed()) {
                 roleId = resultSet.getInt("id_role");
-                resultSet.close();
             }
-            preparedStatement.close();
+            
 
         }catch (SQLException e){
             System.out.println("Role not found");
