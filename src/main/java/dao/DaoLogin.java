824e5818443332ce6ea35fc14142a528a7e63d73
martastaporek <staporekmarta1@gmail.com>
2018-03-08 17:44:18 +0100

    DaoLogin: Fix to work wirh ARM

--- src/main/java/dao/DaoLogin.java
+++ src/main/java/dao/DaoLogin.java
@@ -5,6 +5,7 @@ import model.Admin;
 import model.Mentor;
 import model.Student;
 
+
 import java.sql.*;
 
 public class DaoLogin implements IDaoLogin {
@@ -12,22 +13,18 @@ public class DaoLogin implements IDaoLogin {
     @Override
     public User getUser(String email, String password){
         User user = null;
-        PreparedStatement preparedStatement = null;
+
         String query = "SELECT * FROM users WHERE email= ? AND password= ?;";
-        try{
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
-            preparedStatement.setString(1, email);
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {            preparedStatement.setString(1, email);
             preparedStatement.setString(2, password);
             ResultSet resultSet = preparedStatement.executeQuery();
 
             if(!resultSet.isClosed()) {
                 int id_role = resultSet.getInt("id_role");
                 String role = getRole(id_role);
-
                 user = createUser(resultSet, role);
-                resultSet.close();
             }
-            preparedStatement.close();
 
         }catch(SQLException e){
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
@@ -39,19 +36,19 @@ public class DaoLogin implements IDaoLogin {
     public String getRole(int id_role){
 
         String role = null;
-        PreparedStatement preparedStatement = null;
-        String query = "SELECT name FROM roles WHERE id_role= ?;";
-        try {
-            preparedStatement = DbConnection.getConnection().prepareStatement(query);
-            preparedStatement.setInt(1, id_role);
-            ResultSet resultSet = preparedStatement.executeQuery();
 
-            if(!resultSet.isClosed()) {
-                role = resultSet.getString("name");
-                resultSet.close();
+        String query = "SELECT name FROM roles WHERE id_role= ?;";
+        try (Connection connection = DbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+             preparedStatement.setInt(1, id_role);
+            try(ResultSet resultSet = preparedStatement.executeQuery()) {
+
+                if (!resultSet.isClosed()) {
+                    role = resultSet.getString("name");
+
+                }
             }
 
-            preparedStatement.close();
         } catch (SQLException e) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
         }
