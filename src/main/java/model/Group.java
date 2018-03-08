c15cb147e171f1021bc59084c851492162bfd043
martastaporek <staporekmarta1@gmail.com>
2018-03-08 14:22:06 +0100

    ControllerTeam, Group:  Change declaration ArrayList to List

--- src/main/java/model/Group.java
+++ src/main/java/model/Group.java
@@ -2,15 +2,16 @@ package model;
 
 import java.util.ArrayList;
 import iterator.MyIterator;
+import java.util.List;
 
 public abstract class Group{
     private String name;
-    private ArrayList<Student> students;
+    private List<Student> students;
     private int groupId;
 
     public Group(String name){
         this.name = name;
-        this.students = new ArrayList<Student>();
+        this.students = new ArrayList<>();
     }
 
     public Group(int groupId, String name, ArrayList<Student> students){
@@ -39,7 +40,7 @@ public abstract class Group{
         this.students = students;
     }
 
-    public ArrayList<Student> getStudents(){
+    public List<Student> getStudents(){
         return this.students;
     }
 
