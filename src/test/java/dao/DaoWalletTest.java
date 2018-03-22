5afd3002468b1068c02fe092580ec8b977c266f4
PiotrGry <piotrgryzlo@gmail.com>
2018-03-22 19:14:29 +0100

    Done DaoWalletTest

--- src/test/java/dao/DaoWalletTest.java
+++ src/test/java/dao/DaoWalletTest.java
@@ -1,8 +1,13 @@
 package dao;
 
+import model.Artifact;
 import model.Wallet;
-import org.junit.Before;
 import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -15,25 +20,43 @@ public class DaoWalletTest extends DaoTest{
     }
 
     public void tearDown() {
-
         dao = null;
     }
 
     @Test
-    public void TestIf_WalletNoParametersConstructorIsNotNull() {
+    public void TestIf_createWalletIsNotNull() {
         Wallet wallet = dao.createWallet();
         assertNotNull(wallet);
     }
 
     @Test
-    public void TestIf_WalletConstructorWithParametersWorks() {
-
-        Wallet wallet = new Wallet(0, 0, )
+    public void TestIf_createWalletWithParametersWorks() {
+        List<Artifact> artifacts = new ArrayList<>();
+        List<Artifact> usedArtifacts = new ArrayList<>();
+        int counter = 5;
+        while(counter > 0){
+            artifacts.add(mock(Artifact.class));
+            usedArtifacts.add(mock(Artifact.class));
+            counter--;
+        }
+       Wallet wallet =  dao.createWallet(0, 0, artifacts, usedArtifacts);
+       assertNotNull(wallet);
+       assertEquals(5, wallet.getNewArtifacts().size());
     }
 
     @Test
-    public void importWallet() {
+    public void TestIf_importWallet_returnCorrectObjectFromDB() {
+        Wallet wallet = dao.importWallet(6);
+        assertNotNull(wallet);
+        assertEquals(1, wallet.getUsedArtifacts().size());
+        assertEquals("Item id: 3, name: Time Travel, value: 750, " +
+                                "description: extend SI week assignment deadline by one day, " +
+                                "type: individual",
+                        wallet.getNewArtifacts().get(0).toString());
+
     }
+
+
 
 
 }
