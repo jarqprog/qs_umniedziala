c40aae5d6e68a54295c0ffac3584139e6c8928a6
jarqprog <jarqprog@gmail.com>
2018-04-17 18:48:04 +0200

    refactor

--- src/main/java/server/sessions/ISessionManager.java
+++ src/main/java/server/sessions/ISessionManager.java
@@ -4,7 +4,6 @@ import com.sun.net.httpserver.HttpExchange;
 
 public interface ISessionManager {
 
-//    boolean isLogged(HttpExchange he);  // has user
     boolean validate(HttpExchange he);
     boolean register(HttpExchange he, String userUniqueData);  // userUniqueData - something unique, e.g. email
     boolean remove(HttpExchange he);  // in case of logout
