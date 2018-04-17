798409120c438a6b62f2c750281e93d33a7b711b
Robstepa <zb.arthur@gmail.com>
2018-04-17 15:16:14 +0200

    Refactor

--- src/main/java/server/StudentPage.java
+++ src/main/java/server/StudentPage.java
@@ -8,6 +8,7 @@ public class StudentPage {
     public static void setModel(User user, JtwigModel model){
         model.with("name", user.getName());
         model.with("email", user.getEmail());
+
     }
 
 }
