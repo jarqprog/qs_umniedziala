c9196f6419d8a9326533e8b380ededb5015e6d1e
jarqprog <jarqprog@gmail.com>
2018-05-07 20:06:10 +0200

    add DaoFactory

---
+++ src/main/java/dao/DaoFactory.java
@@ -0,0 +1,66 @@
+package dao;
+
+import manager.database.DatabaseManager;
+
+import java.sql.Connection;
+
+public class DaoFactory implements IDaoFactory {
+
+    private final DatabaseManager dbManager;
+    private Connection connection;
+
+    public static IDaoFactory getInstance(DatabaseManager dbManager) {
+
+        return new DaoFactory(dbManager);
+    }
+
+    private DaoFactory(DatabaseManager dbManager) {
+
+        this.dbManager = dbManager;
+        connection = dbManager.getConnection();
+    }
+
+    public <T extends Dao> T create(Class<T> daoType) {
+
+        if( ! dbManager.isConnectionValid(connection) ) {
+            connection = dbManager.getConnection();
+        }
+
+        String daoName = daoType.getSimpleName();
+        SqlDao dao = null;
+
+        switch(daoName) {
+            case("DaoAdmin"):
+                dao = new DaoAdmin(connection);
+                break;
+            case("DaoArtifact"):
+                dao = new DaoArtifact(connection);
+                break;
+            case("DaoClass"):
+                dao = new DaoClass(connection);
+                break;
+            case("DaoLevel"):
+                dao = new DaoLevel(connection);
+                break;
+            case("DaoLogin"):
+                dao = new DaoLogin(connection);
+                break;
+            case("DaoMentor"):
+                dao = new DaoMentor(connection);
+                break;
+            case("DaoQuest"):
+                dao = new DaoQuest(connection);
+                break;
+            case("DaoStudent"):
+                dao = new DaoStudent(connection);
+                break;
+            case("DaoTeam"):
+                dao = new DaoTeam(connection);
+                break;
+            case("DaoWallet"):
+                dao = new DaoWallet(connection);
+                break;
+        }
+        return daoType.cast(dao);
+    }
+}
